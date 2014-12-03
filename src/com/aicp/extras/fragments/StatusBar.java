e6698d7a28d189143df30aa92ae0e033b4b97800
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 20:56:41 +0200

    Return: Smart Pulldown [2/2]
    Change-Id: Ia2a41a42ca84a30d6353f06c06767d7ec5ba6d08

--- src/com/aicp/extras/fragments/StatusBar.java
+++ src/com/aicp/extras/fragments/StatusBar.java
@@ -17,17 +17,25 @@
 
 package com.aicp.extras.fragments;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 import com.aicp.extras.preference.MasterSwitchPreference;
 
-public class StatusBar extends BaseSettingsFragment {
+public class StatusBar extends BaseSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SMART_PULLDOWN = "qs_smart_pulldown";
 
     MasterSwitchPreference mBatteryBar;
+    private ListPreference mSmartPulldown;
 
     @Override
     protected int getPreferenceResource() {
@@ -37,9 +45,44 @@ public class StatusBar extends BaseSettingsFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         mBatteryBar = (MasterSwitchPreference)
                 findPreference(Settings.System.STATUSBAR_BATTERY_BAR);
+
+        mSmartPulldown = (ListPreference) findPreference(SMART_PULLDOWN);
+        int smartPulldown = Settings.System.getInt(resolver,
+                Settings.System.QS_SMART_PULLDOWN, 0);
+        updateSmartPulldownSummary(smartPulldown);
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSmartPulldown) {
+            int value = Integer.parseInt((String) newValue);
+            updateSmartPulldownSummary(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off_summary));
+        } else if (value == 3) {
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_none_summary));
+        } else {
+            String type = res.getString(value == 1
+                    ? R.string.smart_pulldown_dismissable
+                    : R.string.smart_pulldown_ongoing);
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+        }
     }
 
     @Override
