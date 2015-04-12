f2fae8b805d7cd87cff8a6377988efc0e86e1637
Lokesh Chamane <lokesh.c703@gmail.com>
2015-04-12 08:16:00 +0000

    AE: HeadsUp snooze function [2/2]
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Change-Id: If92d0635f07caa3a40a620ee4730f39309e4bf97

--- src/com/lordclockan/aicpextras/HeadsUpSettingsFragment.java
+++ src/com/lordclockan/aicpextras/HeadsUpSettingsFragment.java
@@ -38,8 +38,10 @@ public class HeadsUpSettingsFragment extends Fragment {
         }
 
         private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+        private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
 
         private ListPreference mHeadsUpTimeOut;
+        private ListPreference mHeadsUpSnoozeTime;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -67,6 +69,14 @@ public class HeadsUpSettingsFragment extends Fragment {
             mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
             updateHeadsUpTimeOutSummary(headsUpTimeOut);
 
+            int defaultSnooze = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                       "com.android.systemui:integer/heads_up_default_snooze_length_ms", null, null));
+            mHeadsUpSnoozeTime = (ListPreference) findPreference(PREF_HEADS_UP_SNOOZE_TIME);
+            mHeadsUpSnoozeTime.setOnPreferenceChangeListener(this);
+            int headsUpSnooze = Settings.System.getInt(resolver,
+                   Settings.System.HEADS_UP_NOTIFICATION_SNOOZE, defaultSnooze);
+            mHeadsUpSnoozeTime.setValue(String.valueOf(headsUpSnooze));
+            updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
         }
 
         @Override
@@ -79,6 +89,13 @@ public class HeadsUpSettingsFragment extends Fragment {
                         headsUpTimeOut);
                 updateHeadsUpTimeOutSummary(headsUpTimeOut);
                 return true;
+            } else if (preference == mHeadsUpSnoozeTime) {
+                int headsUpSnooze = Integer.valueOf((String) newValue);
+                Settings.System.putInt(resolver,
+                        Settings.System.HEADS_UP_NOTIFICATION_SNOOZE,
+                        headsUpSnooze);
+                updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
+                return true;
             }
             return false;
         }
@@ -87,6 +104,17 @@ public class HeadsUpSettingsFragment extends Fragment {
             String summary = getResources().getString(R.string.heads_up_time_out_summary,
                     value / 1000);
             mHeadsUpTimeOut.setSummary(summary);
+        }
+
+        private void updateHeadsUpSnoozeTimeSummary(int value) {
+            if (value == 0) {
+                mHeadsUpSnoozeTime.setSummary(getResources().getString(R.string.heads_up_snooze_disabled_summary));
+            } else if (value == 60000) {
+                mHeadsUpSnoozeTime.setSummary(getResources().getString(R.string.heads_up_snooze_summary_one_minute));
+            } else {
+                String summary = getResources().getString(R.string.heads_up_snooze_summary, value / 60 / 1000);
+                mHeadsUpSnoozeTime.setSummary(summary);
+            }
         }
 
         @Override
