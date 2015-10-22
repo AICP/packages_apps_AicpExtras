3043ebaeb9708e95e027853f490afcd0a62f42f0
Hector Colmenares <hcolmenares@gmail.com>
2015-10-21 21:27:33 -0400

    AicpExtras: Let's build it
    Change-Id: Ie7b7619a246be8e3b057ae3c82c67df9f0b1b11b

--- Android.mk
+++ Android.mk
@@ -1,23 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_STATIC_JAVA_LIBRARIES := support-v4-23.1.0-sources cardview-v7-23.1.0-sources appcompat-v7-23.1.0-sources support-annotations-23.1.0-sources
+LOCAL_PACKAGE_NAME := AicpExtras
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PACKAGE_NAME := AicpExtras
+LOCAL_STATIC_JAVA_LIBRARIES :=  \
+   android-support-v4           \
+   android-support-v7-appcompat \
+   android-support-v7-cardview  \
+   android-support-design       \
+   android-support-annotations
 
+LOCAL_RESOURCE_DIR = \
+        $(LOCAL_PATH)/res \
+        frameworks/support/v7/appcompat/res \
+        frameworks/support/design/res 
+
+LOCAL_AAPT_FLAGS := \
+        --auto-add-overlay \
+        --extra-packages android.support.v7.appcompat \
+        --extra-packages android.support.design 
 
 include $(BUILD_PACKAGE)
-##################################################
-include $(CLEAR_VARS)
 
-# LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := roottools:libs/RootTools.jar renderscript:libs/renderscript-v8.jar google:libs/libGoogleAnalyticsServices.jar nineoldandroids:libs/nineoldandroids-2.4.0.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
-# Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
