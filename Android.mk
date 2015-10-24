67be4cb605b4855dc8b4a3bcb0f193f17aa81e9f
Hector A <hcolmenares@gmail.com>
2015-10-24 20:38:34 +0200

    Revert "AicpExtras: Let's build it"
    This reverts commit e85de26f3b9080b66a80cadbb4447b8a326f801c.
    
    Change-Id: If8e7e4fd392577b0c3eb93db1af7a61f5f09b8cc

--- Android.mk
+++ Android.mk
@@ -1,29 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := support-v4-23.1.0-sources cardview-v7-23.1.0-sources appcompat-v7-23.1.0-sources support-annotations-23.1.0-sources
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES :=   \
-    android-support-v7_23-appcompat \
-    android-support-v4_23           \
-    android-support-v7_23-cardview  \
-    android-support-annotations_23  \
-    android-support-design_23
-
-LOCAL_PACKAGE_NAME := AicpExtras
-LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := AicpExtras
 
-#LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
-
-# Support library v4
+##################################################
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := 	\
-    android-support-v7-appcompat:libs/appcompat-v7-23.1.0-sources.jar         \
-    android-support-v7-cardview:libs/cardview-v7-23.1.0-sources.jar           \
-    android-support-design_23:libs/design-23.1.0-sources.jar
+# LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := roottools:libs/RootTools.jar renderscript:libs/renderscript-v8.jar google:libs/libGoogleAnalyticsServices.jar nineoldandroids:libs/nineoldandroids-2.4.0.jar
 
 include $(BUILD_MULTI_PREBUILT)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
