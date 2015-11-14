db3985bf8d671d86965503d7bbfdc05b646e25ab
LorDClockaN <davor@losinj.com>
2015-11-14 21:03:15 +0100

    Toast icon switch [1/2]
    - Turn off by default
    
    Also clean up Display fragment from dummy stuff
    
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- src/com/lordclockan/aicpextras/DisplayAnimationsActivity.java
+++ src/com/lordclockan/aicpextras/DisplayAnimationsActivity.java
@@ -23,10 +23,6 @@ public class DisplayAnimationsActivity extends Fragment {
     }
 
     public static class SettingsPreferenceFragment extends PreferenceFragment {
-        private String TEST_CHECK = "test_check";
-        private String FUCK_AMOUNT = "fuck_amount";
-        private SwitchPreference mTestCheck;
-        private ListPreference mFuckAmount;
         public SettingsPreferenceFragment() {
         }
 
@@ -41,47 +37,6 @@ public class DisplayAnimationsActivity extends Fragment {
             Activity activity = getActivity();
 
             final ContentResolver resolver = getActivity().getContentResolver();
-
-            mTestCheck = (SwitchPreference) findPreference(TEST_CHECK);
-
-            mFuckAmount = (ListPreference) findPreference(FUCK_AMOUNT);
-            mFuckAmount.setSummary(mFuckAmount.getEntry());
-
-            addListener();
-
-
-        }
-
-        public void addListener() {
-            mTestCheck.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if (preference == mTestCheck) {
-                        boolean value = (Boolean) newValue;
-                        if (value) {
-                            Toast.makeText(getActivity(), "CheckBox is ON", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getActivity(), "CheckBox is OFF", Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
-                    }
-                    return false;
-                }
-            });
-
-            mFuckAmount.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if (preference == mFuckAmount) {
-                        //int fuckAmount = Integer.valueOf((String) newValue);
-                        int index = mFuckAmount.findIndexOfValue((String) newValue);
-                        mFuckAmount.setSummary(mFuckAmount.getEntries()[index]);
-                        Toast.makeText(getActivity(), mFuckAmount.getSummary(), Toast.LENGTH_SHORT).show();
-                        return true;
-                    }
-                    return false;
-                }
-            });
         }
     }
 }
