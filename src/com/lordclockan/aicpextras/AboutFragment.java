2cbc7d38e4367f6e252ebfb25316209f13158ef5
LorDClockaN <davor@losinj.com>
2015-11-14 14:50:21 +0100

    Change About to fragment and add ChangeLog from main settings
    -From here:
    https://github.com/AICP/packages_apps_Settings/commit/15003a1726ff4a0e6c9206834f8e789265652702
    
    -ChangeLog fragment transformed into activity so it can be
    opened more easily
    
    Change-Id: Iafde97c6c4de9881163de07d110efd72247654c1

--- src/com/lordclockan/aicpextras/AboutFragment.java
+++ src/com/lordclockan/aicpextras/AboutFragment.java
@@ -1,66 +1,84 @@
 package com.lordclockan.aicpextras;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-/**
- * Created by lord on 07.10.15..
- */
 public class AboutFragment extends Fragment {
 
-    View myView;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.about_layout, container, false);
-        addListenerOnGCommunity();
-        return myView;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new SettingsPreferenceFragment())
+                .commit();
     }
 
-    public void addListenerOnGCommunity() {
+    public static class SettingsPreferenceFragment extends PreferenceFragment {
 
-        TextView mGcommunty = (TextView) myView.findViewById(R.id.tvGplus);
-        mGcommunty.setOnClickListener(new View.OnClickListener() {
+        private String PREF_GCOMMUNITY = "gplus";
+        private String PREF_AICP_DOWNLOADS = "aicp_downloads";
+        private String PREF_AICP_GERRIT = "aicp_gerrit";
+        private String PREF_AICP_CHANGELOG = "aicp_changelog";
 
-            @Override
-            public void onClick(View arg0) {
+        private Preference mGcommunity;
+        private Preference mAicpDownloads;
+        private Preference mAicpGerrit;
+        private Preference mAicpChangeLog;
+        public SettingsPreferenceFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.about_layout);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            Activity activity = getActivity();
+
+            final ContentResolver resolver = getActivity().getContentResolver();
+
+            mGcommunity = prefSet.findPreference(PREF_GCOMMUNITY);
+            mAicpDownloads = prefSet.findPreference(PREF_AICP_DOWNLOADS);
+            mAicpGerrit = prefSet.findPreference(PREF_AICP_GERRIT);
+            mAicpChangeLog = prefSet.findPreference(PREF_AICP_CHANGELOG);
+
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            if (preference == mGcommunity) {
                 String url = "https://plus.google.com/communities/101008638920580274588";
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
-            }
-        });
-
-        TextView mAicpDownloads = (TextView) myView.findViewById(R.id.tvAicpDownloads);
-        mAicpDownloads.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
+            } else if (preference == mAicpDownloads) {
                 String url = "http://dwnld.aicp-rom.com";
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
-            }
-        });
-
-        TextView mAicpGerrit = (TextView) myView.findViewById(R.id.tvAicpGerrit);
-        mAicpGerrit.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
+            } else if (preference == mAicpGerrit) {
                 String url = "http://gerrit.aicp-rom.com/#/q/status:open";
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
+            } else if (preference == mAicpChangeLog) {
+                Intent intent = new Intent(getActivity(), ChangeLogActivity.class);
+                getActivity().startActivity(intent);
+            } else {
+                return super.onPreferenceTreeClick(preferenceScreen, preference);
             }
-        });
+
+            return false;
+        }
     }
 }
---
+++ src/com/lordclockan/aicpextras/ChangeLogActivity.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2015 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.lordclockan.aicpextras.R;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ChangeLogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            InputStreamReader inputReader = null;
+            String text = null;
+
+            try {
+                StringBuilder data = new StringBuilder();
+                char tmp[] = new char[2048];
+                int numRead;
+
+                inputReader = new FileReader(CHANGELOG_PATH);
+                while ((numRead = inputReader.read(tmp)) >= 0) {
+                    data.append(tmp, 0, numRead);
+                }
+                text = data.toString();
+            } catch (IOException e) {
+                text = getString(R.string.changelog_error);
+            } finally {
+                try {
+                    if (inputReader != null) {
+                        inputReader.close();
+                    }
+                } catch (IOException e) {
+                }
+            }
+
+            final TextView textView = new TextView(getActivity());
+            textView.setText(text);
+
+            final ScrollView scrollView = new ScrollView(getActivity());
+            scrollView.addView(textView);
+
+            return scrollView;
+        }
+    }
+}
