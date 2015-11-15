391e2e0857545e8bfbcb46cb6cb8d33c78256c0a
LorDClockaN <davor@losinj.com>
2015-11-15 06:45:12 +0100

    Turn all other dummy activities into Fragments
    Customization mod set to recieve :D
    
    Change-Id: I5fb0d71f5ddab6a052649f39756a1d2bb5c77471

--- src/com/lordclockan/aicpextras/StatusBarFragment.java
+++ src/com/lordclockan/aicpextras/StatusBarFragment.java
@@ -1,23 +1,42 @@
 package com.lordclockan.aicpextras;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * Created by lord on 07.10.15..
- */
 public class StatusBarFragment extends Fragment {
 
-    View myView;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.ui_layout, container, false);
-        return myView;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new SettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class SettingsPreferenceFragment extends PreferenceFragment {
+        public SettingsPreferenceFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.ui_layout);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            Activity activity = getActivity();
+
+            final ContentResolver resolver = getActivity().getContentResolver();
+        }
     }
 }
