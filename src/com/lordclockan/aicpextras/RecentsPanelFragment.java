db4704934779668a5f392045889af9f9f50b9538
Cristiano Matos <cristianobmatos@gmail.com>
2015-11-16 14:22:16 +0100

    RecentSettings: recents panel clear all button tweaks
    * Show or hide recents button
    * Place the button in top left, center, right or bottom left, center, right
    
    AICPfy:
    Add recents_panel_layout.xml for the first time now
    
    Change-Id: Ic768c3eaba47e0bbb26429fcc5de4449664add7b

--- src/com/lordclockan/aicpextras/RecentsPanelFragment.java
+++ src/com/lordclockan/aicpextras/RecentsPanelFragment.java
@@ -2,12 +2,19 @@ package com.lordclockan.aicpextras;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.widget.Toast;
 
@@ -22,21 +29,49 @@ public class RecentsPanelFragment extends Fragment {
                 .commit();
     }
 
-    public static class SettingsPreferenceFragment extends PreferenceFragment {
+    public static class SettingsPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
         public SettingsPreferenceFragment() {
         }
+
+        private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        private SwitchPreference mRecentsClearAll;
+        private ListPreference mRecentsClearAllLocation;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.ui_layout);
+            addPreferencesFromResource(R.xml.recents_panel_layout);
 
             PreferenceScreen prefSet = getPreferenceScreen();
             Activity activity = getActivity();
+            ContentResolver resolver = getActivity().getContentResolver();
 
-            final ContentResolver resolver = getActivity().getContentResolver();
+            mRecentsClearAllLocation = (ListPreference) prefSet.findPreference(RECENTS_CLEAR_ALL_LOCATION);
+            int location = Settings.System.getIntForUser(resolver,
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setValue(String.valueOf(location));
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+            mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mRecentsClearAllLocation) {
+                int location = Integer.valueOf((String) newValue);
+                int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+                mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+                return true;
+            }
+            return false;
         }
     }
 }
