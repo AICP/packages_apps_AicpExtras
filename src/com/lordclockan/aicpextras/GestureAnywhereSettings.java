c5a558be9d36e9659668b3e6fa88307158215305
LorDClockaN <davor@losinj.com>
2015-11-29 14:40:49 +0100

    Sort out some PreferenceActivities:
    - Change PreferenceActivities with seekbars to
    AppCompatActivity, theme them nicely so they get
    Appbar with title
    - Sexy dialog theming
    - Export all colors so it can be themed with CMTE
    - Clean up a bit
    
    Change-Id: Ib1763c422dd2f0d91c6781cf132becb12cd21c98

--- src/com/lordclockan/aicpextras/GestureAnywhereSettings.java
+++ src/com/lordclockan/aicpextras/GestureAnywhereSettings.java
@@ -17,206 +17,153 @@
 package com.lordclockan.aicpextras;
 
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.ContentObserver;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
-import android.telephony.TelephonyManager;
-import android.text.Spannable;
-import android.text.TextUtils;
-import android.provider.Settings.SettingNotFoundException;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import com.lordclockan.aicpextras.R;
 import com.lordclockan.aicpextras.widget.SeekBarPreferenceCham;
 
-public class GestureAnywhereSettings extends PreferenceActivity
-        implements OnPreferenceChangeListener {
-
-    private static final String TAG = "GestureAnywhereSettings";
-
-    private AppCompatDelegate mDelegate;
-
-    private static final String KEY_ENABLED = "gesture_anywhere_enabled";
-    private static final String KEY_POSITION = "gesture_anywhere_position";
-    private static final String KEY_GESTURES = "gesture_anywhere_gestures";
-    private static final String KEY_TRIGGER_WIDTH = "gesture_anywhere_trigger_width";
-    private static final String KEY_TRIGGER_TOP = "gesture_anywhere_trigger_top";
-    private static final String KEY_TRIGGER_BOTTOM = "gesture_anywhere_trigger_bottom";
-
-    private SwitchPreference mEnabledPref;
-    private ListPreference mPositionPref;
-    private SeekBarPreferenceCham mTriggerWidthPref;
-    private SeekBarPreferenceCham mTriggerTopPref;
-    private SeekBarPreferenceCham mTriggerBottomPref;
-    private Preference mGestures;
-
-    private CharSequence mPreviousTitle;
+public class GestureAnywhereSettings extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        getDelegate().installViewFactory();
-        getDelegate().onCreate(savedInstanceState);
+    protected void onCreate(final Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Toolbar actionBarToolbar = (Toolbar) this.findViewById(R.id.toolbar);
-        actionBarToolbar.setTitle(R.string.gesture_anywhere_title);
-
-        addPreferencesFromResource(R.xml.gesture_anywhere);
-
-        ContentResolver resolver = this.getContentResolver();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setVisibility(View.GONE);
-
-        mEnabledPref = (SwitchPreference) findPreference(KEY_ENABLED);
-        mEnabledPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_ENABLED, 0) == 1));
-        mEnabledPref.setOnPreferenceChangeListener(this);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-        mPositionPref = (ListPreference) prefSet.findPreference(KEY_POSITION);
-        mPositionPref.setOnPreferenceChangeListener(this);
-        int position = Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_POSITION, Gravity.LEFT);
-        mPositionPref.setValue(String.valueOf(position));
-        updatePositionSummary(position);
-
-        mTriggerWidthPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_WIDTH);
-        mTriggerWidthPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 40));
-        mTriggerWidthPref.setOnPreferenceChangeListener(this);
-
-        mTriggerTopPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_TOP);
-        mTriggerTopPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_TRIGGER_TOP, 0));
-        mTriggerTopPref.setOnPreferenceChangeListener(this);
-
-        mTriggerBottomPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_BOTTOM);
-        mTriggerBottomPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_TRIGGER_HEIGHT, 100));
-        mTriggerBottomPref.setOnPreferenceChangeListener(this);
-
-        mGestures = prefSet.findPreference(KEY_GESTURES);
-
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new GestureAnywhereSettingsFragment()).commit();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mPositionPref) {
-            int position = Integer.valueOf((String) newValue);
+    public static class GestureAnywhereSettingsFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        private static final String TAG = "GestureAnywhereSettings";
+
+
+        private static final String KEY_ENABLED = "gesture_anywhere_enabled";
+        private static final String KEY_POSITION = "gesture_anywhere_position";
+        private static final String KEY_GESTURES = "gesture_anywhere_gestures";
+        private static final String KEY_TRIGGER_WIDTH = "gesture_anywhere_trigger_width";
+        private static final String KEY_TRIGGER_TOP = "gesture_anywhere_trigger_top";
+        private static final String KEY_TRIGGER_BOTTOM = "gesture_anywhere_trigger_bottom";
+
+        private SwitchPreference mEnabledPref;
+        private ListPreference mPositionPref;
+        private SeekBarPreferenceCham mTriggerWidthPref;
+        private SeekBarPreferenceCham mTriggerTopPref;
+        private SeekBarPreferenceCham mTriggerBottomPref;
+        private Preference mGestures;
+
+        private CharSequence mPreviousTitle;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.gesture_anywhere);
+
+            ContentResolver resolver = getActivity().getContentResolver();
+
+            mEnabledPref = (SwitchPreference) findPreference(KEY_ENABLED);
+            mEnabledPref.setChecked((Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_ENABLED, 0) == 1));
+            mEnabledPref.setOnPreferenceChangeListener(this);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            mPositionPref = (ListPreference) prefSet.findPreference(KEY_POSITION);
+            mPositionPref.setOnPreferenceChangeListener(this);
+            int position = Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_POSITION, Gravity.LEFT);
+            mPositionPref.setValue(String.valueOf(position));
             updatePositionSummary(position);
-            return true;
-        } else if (preference == mEnabledPref) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.GESTURE_ANYWHERE_ENABLED,
-                    ((Boolean) newValue).booleanValue() ? 1 : 0);
-            return true;
-        } else if (preference == mTriggerWidthPref) {
-            int width = ((Integer)newValue).intValue();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, width);
-            return true;
-        } else if (preference == mTriggerTopPref) {
-            int top = ((Integer)newValue).intValue();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.GESTURE_ANYWHERE_TRIGGER_TOP, top);
-            return true;
-        } else if (preference == mTriggerBottomPref) {
-            int bottom = ((Integer)newValue).intValue();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.GESTURE_ANYWHERE_TRIGGER_HEIGHT, bottom);
-            return true;
+
+            mTriggerWidthPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_WIDTH);
+            mTriggerWidthPref.setValue(Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 40));
+            mTriggerWidthPref.setOnPreferenceChangeListener(this);
+
+            mTriggerTopPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_TOP);
+            mTriggerTopPref.setValue(Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_TRIGGER_TOP, 0));
+            mTriggerTopPref.setOnPreferenceChangeListener(this);
+
+            mTriggerBottomPref = (SeekBarPreferenceCham) findPreference(KEY_TRIGGER_BOTTOM);
+            mTriggerBottomPref.setValue(Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_TRIGGER_HEIGHT, 100));
+            mTriggerBottomPref.setOnPreferenceChangeListener(this);
+
+            mGestures = prefSet.findPreference(KEY_GESTURES);
+
         }
-        return false;
-    }
 
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mGestures) {
-            Intent intent = new Intent(this, GestureAnywhereBuilderActivity.class);
-            this.startActivity(intent);
-        } else {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mPositionPref) {
+                int position = Integer.valueOf((String) newValue);
+                updatePositionSummary(position);
+                return true;
+            } else if (preference == mEnabledPref) {
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_ENABLED,
+                        ((Boolean) newValue).booleanValue() ? 1 : 0);
+                return true;
+            } else if (preference == mTriggerWidthPref) {
+                int width = ((Integer) newValue).intValue();
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, width);
+                return true;
+            } else if (preference == mTriggerTopPref) {
+                int top = ((Integer) newValue).intValue();
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_TRIGGER_TOP, top);
+                return true;
+            } else if (preference == mTriggerBottomPref) {
+                int bottom = ((Integer) newValue).intValue();
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_TRIGGER_HEIGHT, bottom);
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    private void updatePositionSummary(int value) {
-        mPositionPref.setSummary(mPositionPref.getEntries()[mPositionPref.findIndexOfValue("" + value)]);
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_POSITION, value);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 0);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 1);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        getDelegate().onPostCreate(savedInstanceState);
-    }
-
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        getDelegate().setContentView(layoutResID);
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        getDelegate().onPostResume();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        getDelegate().onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        getDelegate().onDestroy();
-    }
-
-    private void setSupportActionBar(@Nullable Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
-    }
-
-    private AppCompatDelegate getDelegate() {
-        if (mDelegate == null) {
-            mDelegate = AppCompatDelegate.create(this, null);
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            if (preference == mGestures) {
+                Intent intent = new Intent(getActivity(), GestureAnywhereBuilderActivity.class);
+                getActivity().startActivity(intent);
+            } else {
+                return super.onPreferenceTreeClick(preferenceScreen, preference);
+            }
+            return false;
         }
-        return mDelegate;
-    }
+
+        private void updatePositionSummary(int value) {
+            mPositionPref.setSummary(mPositionPref.getEntries()[mPositionPref.findIndexOfValue("" + value)]);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.GESTURE_ANYWHERE_POSITION, value);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 0);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 1);
+        }
+
+    }    
 }
