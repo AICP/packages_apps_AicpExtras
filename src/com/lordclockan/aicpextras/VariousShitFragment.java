6f66b4c469fd6b87822f544eaee15f5a645a3d3d
LorDClockaN <davor@losinj.com>
2015-12-12 21:39:47 +0100

    Add link to SystemUI Tuner
    Dialog will appear only on first run with more info
    about SystemUI tuner
    Using getSharedPreferences for this, there is probably
    more nicer way of coding this, but this works fine
    
    Change-Id: If3887a6071de4eccf14258c6e7b09231e38a0e1c

--- src/com/lordclockan/aicpextras/VariousShitFragment.java
+++ src/com/lordclockan/aicpextras/VariousShitFragment.java
@@ -1,8 +1,13 @@
 package com.lordclockan.aicpextras;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemProperties;
@@ -13,6 +18,7 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 import com.lordclockan.R;
 
@@ -33,11 +39,23 @@ public class VariousShitFragment extends Fragment {
         public SettingsPreferenceFragment() {
         }
 
+        private SharedPreferences mVariousShitSettings;
+        private Editor toEditVariousShit;
+
         private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
         private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
         private static final String SCROLLINGCACHE_DEFAULT = "1";
+        private static final String PREF_SYSTEMUI_TUNER = "systemui_tuner";
+
+        // Package name of the SystemUI tuner
+        public static final String SYSTEMUITUNER_PACKAGE_NAME = "com.android.systemui";
+        // Intent for launching the SystemUI tuner actvity
+        public static Intent INTENT_SYSTEMUITUNER_SETTINGS = new Intent(Intent.ACTION_MAIN)
+                .setClassName(SYSTEMUITUNER_PACKAGE_NAME, SYSTEMUITUNER_PACKAGE_NAME + ".tuner.TunerActivity");
 
         private ListPreference mScrollingCachePref;
+        private Preference mSystemUITuner;
+        private String mTunerFirstRun = "true";
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -49,10 +67,16 @@ public class VariousShitFragment extends Fragment {
             PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
 
+            mVariousShitSettings = getActivity().getSharedPreferences("SystemUITuner", Context.MODE_PRIVATE);
+
             mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
             mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
                     SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
             mScrollingCachePref.setOnPreferenceChangeListener(this);
+
+            mSystemUITuner = prefSet.findPreference(PREF_SYSTEMUI_TUNER);
+
+            sharedPreferences();
 
         }
 
@@ -70,10 +94,42 @@ public class VariousShitFragment extends Fragment {
 
         @Override
         public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-            /*} else {
+            if (preference == mSystemUITuner) {
+                if (mVariousShitSettings.getString("firstRun", "").equals("true")) {
+                    toEditVariousShit = mVariousShitSettings.edit();
+                    toEditVariousShit.putString("firstRun", "false");
+                    toEditVariousShit.commit();
+                    mTunerFirstRun = "false";
+                    systemUITunerDialogWarning();
+                } else if (mVariousShitSettings.getString("firstRun", "").equals("false")) {
+                    getActivity().startActivity(INTENT_SYSTEMUITUNER_SETTINGS);
+                }
+            } else {
                 return super.onPreferenceTreeClick(preferenceScreen, preference);
-            }*/
+            }
             return false;
+        }
+
+        public void sharedPreferences() {
+            toEditVariousShit = mVariousShitSettings.edit();
+            toEditVariousShit.putString("firstRun", mTunerFirstRun);
+            toEditVariousShit.commit();
+        }
+
+        public void systemUITunerDialogWarning() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.systemui_tuner_dilog_title);
+            builder.setMessage(R.string.systemui_tuner_dilog_warning);
+
+            builder.setPositiveButton(R.string.continue_title, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    getActivity().startActivity(INTENT_SYSTEMUITUNER_SETTINGS);
+                }
+             });
+
+             AlertDialog alertDialog = builder.create();
+             alertDialog.show();
         }
     }
 }
