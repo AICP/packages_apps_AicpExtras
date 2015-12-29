26b0bd20eb785b6cbb7a4f58553bf1d0984e0c26
LorDClockaN <davor@losinj.com>
2015-12-29 21:31:38 +0530

    AICP Logo on Status Bar (2/2)
    PS2: Code fixup
    
    Change-Id: Ia6309c8039cc3484b0220527d741cf26c6c4fa9e

--- src/com/lordclockan/aicpextras/StatusBarFragment.java
+++ src/com/lordclockan/aicpextras/StatusBarFragment.java
@@ -7,8 +7,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import android.support.v4.app.Fragment;
 
 import com.lordclockan.R;
@@ -24,7 +28,8 @@ public class StatusBarFragment extends Fragment {
                 .commit();
     }
 
-    public static class SettingsPreferenceFragment extends PreferenceFragment {
+    public static class SettingsPreferenceFragment extends PreferenceFragment 
+            implements OnPreferenceChangeListener {
 
         public SettingsPreferenceFragment() {
         }
@@ -33,11 +38,13 @@ public class StatusBarFragment extends Fragment {
         private String PREF_CARRIE_LABEL = "carrierlabel";
         private String PREF_BATTERY_BAR = "batterybar";
         private String PREF_STATUSBAR_WEATHER = "statusbar_weather";
+        private static final String KEY_AICP_LOGO_COLOR = "status_bar_aicp_logo_color";
 
         private Preference mTraffic;
         private Preference mCarrierLabel;
         private Preference mBatteryBar;
         private Preference mStatusbarWeather;
+        private ColorPickerPreference mAicpLogoColor;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -47,12 +54,23 @@ public class StatusBarFragment extends Fragment {
             addPreferencesFromResource(R.xml.statusbar_layout);
 
             PreferenceScreen prefSet = getPreferenceScreen();
-            ContentResolver resolver = getActivity().getContentResolver();
+            final ContentResolver resolver = getActivity().getContentResolver();
 
             mTraffic = prefSet.findPreference(PREF_TRAFFIC);
             mCarrierLabel = prefSet.findPreference(PREF_CARRIE_LABEL);
             mBatteryBar = prefSet.findPreference(PREF_BATTERY_BAR);
             mStatusbarWeather = prefSet.findPreference(PREF_STATUSBAR_WEATHER);
+
+            // Aicp logo color
+            mAicpLogoColor =
+                (ColorPickerPreference) prefSet.findPreference(KEY_AICP_LOGO_COLOR);
+            mAicpLogoColor.setOnPreferenceChangeListener(this);
+            int intColor = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_AICP_LOGO_COLOR, 0xffffffff);
+            String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mAicpLogoColor.setSummary(hexColor);
+            mAicpLogoColor.setNewPreviewColor(intColor);
+
         }
 
         @Override
@@ -71,6 +89,21 @@ public class StatusBarFragment extends Fragment {
                 getActivity().startActivity(intent);
             } else {
                 return super.onPreferenceTreeClick(preferenceScreen, preference);
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mAicpLogoColor) {
+                String hex = ColorPickerPreference.convertToARGB(
+                        Integer.valueOf(String.valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(resolver,
+                        Settings.System.STATUS_BAR_AICP_LOGO_COLOR, intHex);
+                return true;
             }
             return false;
         }
