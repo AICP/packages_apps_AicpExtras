8dd53d8b1dbe09af540fa6dcbd31aa169f1e6bcf
LorDClockaN <davor@losinj.com>
2015-12-29 21:31:38 +0530

    AICP Logo on Status Bar (2/2)
    Change-Id: Ia6309c8039cc3484b0220527d741cf26c6c4fa9e

--- src/com/lordclockan/aicpextras/StatusBarFragment.java
+++ src/com/lordclockan/aicpextras/StatusBarFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import android.preference.PreferenceScreen;
 import android.support.v4.app.Fragment;
 
@@ -33,11 +34,13 @@ public class StatusBarFragment extends Fragment {
         private String PREF_CARRIE_LABEL = "carrierlabel";
         private String PREF_BATTERY_BAR = "batterybar";
         private String PREF_STATUSBAR_WEATHER = "statusbar_weather";
+        private static final String KEY_AICP_LOGO_COLOR = "status_bar_aicp_logo_color";
 
         private Preference mTraffic;
         private Preference mCarrierLabel;
         private Preference mBatteryBar;
         private Preference mStatusbarWeather;
+        private ColorPickerPreference mAicpLogoColor;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,17 @@ public class StatusBarFragment extends Fragment {
             mCarrierLabel = prefSet.findPreference(PREF_CARRIE_LABEL);
             mBatteryBar = prefSet.findPreference(PREF_BATTERY_BAR);
             mStatusbarWeather = prefSet.findPreference(PREF_STATUSBAR_WEATHER);
+
+        // Aicp logo color
+        mAicpLogoColor =
+            (ColorPickerPreference) prefSet.findPreference(KEY_AICP_LOGO_COLOR);
+        mAicpLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_AICP_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mAicpLogoColor.setSummary(hexColor);
+            mAicpLogoColor.setNewPreviewColor(intColor);
+
         }
 
         @Override
@@ -71,6 +85,14 @@ public class StatusBarFragment extends Fragment {
                 getActivity().startActivity(intent);
             } else {
                 return super.onPreferenceTreeClick(preferenceScreen, preference);
+            } else if (preference == mAicpLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_AICP_LOGO_COLOR, intHex);
+            return true;
             }
             return false;
         }
