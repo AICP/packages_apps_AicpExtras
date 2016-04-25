2462a80e890af60690b84d4493dc60dd937d511f
rogersb11 <brettrogers11@gmail.com>
2016-04-25 17:01:06 +0200

    Floating Windows settings
    AICPfy
    
    Change-Id: Ibfc7d2c29001597942753a465600cc03a1486abf
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ src/com/lordclockan/aicpextras/FloatingWindows.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 VRToxin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lordclockan.aicpextras;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lordclockan.R;
+
+public class FloatingWindows extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new FloatingWindowsFragment()).commit();
+    }
+
+    public static class FloatingWindowsFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        private static final String TAG = "FloatingWindows";
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            return false;
+        }
+    }
+}
