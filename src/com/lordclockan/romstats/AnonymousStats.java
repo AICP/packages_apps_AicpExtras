ec406e141b2a52fc6ee8d1f1c3c6b67487b8404c
pranav <cherukupallip@gmail.com>
2016-05-30 19:58:43 +0530

    AicpExtras: fix romstats url
    Change-Id: Iff87a7fc5fa8d0bf457d5ab88ad57bdde7a25e2f

--- src/com/lordclockan/romstats/AnonymousStats.java
+++ src/com/lordclockan/romstats/AnonymousStats.java
@@ -174,7 +174,7 @@ public class AnonymousStats extends PreferenceActivity implements
 			}
 		} else if (preference == mViewStats) {
 			// Display the stats page
-			Uri uri = Uri.parse(Utilities.getStatsUrl() + "stats");
+			Uri uri = Uri.parse(Utilities.getStatsUrl());
 			startActivity(new Intent(Intent.ACTION_VIEW, uri));
 		} else {
 			// If we didn't handle it, let preferences handle it.
