8df04ec7d0f5143514a02b652d10035ffddeca29
pranav <cherukupallip@gmail.com>
2016-06-01 21:56:51 +0530

    AicpExtras:  press back twice to exit app
    *this does not affect the naviation drawer, it still closes on first
     time
    
    Change-Id: I8f7600abcadec84caae22b36e4ca03df36939615

--- src/com/lordclockan/aicpextras/MainActivity.java
+++ src/com/lordclockan/aicpextras/MainActivity.java
@@ -19,6 +19,8 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.lang.System;
+
 import com.lordclockan.R;
 
 public class MainActivity extends AppCompatActivity
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity
 
     private DrawerLayout mDrawer;
     private int id;
+    private long startTime;
+    private boolean secondBack=false;
 
     @Override
     protected void onStart() {
@@ -119,7 +123,22 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            /*
+            **If back was pressed the first time,only show toast
+            **else exit the app provided the user presses back
+            **the second time within 2.5 seconds of the first
+            */
+            if(!secondBack) {
+            Toast.makeText(MainActivity.this, "Press back again to exit",
+            Toast.LENGTH_SHORT).show();
+            secondBack=!secondBack;
+            startTime = System.currentTimeMillis();
+            } else if( System.currentTimeMillis()-startTime < 2500)
             super.onBackPressed();
+            else {
+            secondBack=!secondBack;
+            onBackPressed();
+           }
         }
     }
 
