2dedca3648eb71e7708c508e3e17204db5c899b4
LorDClockaN <lordclockan@gmail.com>
2016-07-24 17:25:40 +0200

    AE: Notifications transparency (2/2)
    Change-Id: Ife852292276c0aaf052e1febb55ddc297a3c2ae6
    Signed-off-by: LorDClockaN <lordclockan@gmail.com>

--- src/com/lordclockan/aicpextras/TransparencyPornFragment.java
+++ src/com/lordclockan/aicpextras/TransparencyPornFragment.java
@@ -55,6 +55,7 @@ public class TransparencyPornFragment extends Fragment {
         private static final String PREF_QS_CORNER_RADIUS = "qs_corner_radius";
         private static final String PREF_QS_STROKE_DASH_WIDTH = "qs_dash_width";
         private static final String PREF_QS_STROKE_DASH_GAP = "qs_dash_gap";
+        private static final String PREF_NOTIFICATION_ALPHA = "notification_alpha";
 
         private SeekBarPreferenceCham mQSShadeAlpha;
         private SeekBarPreferenceCham mQSHeaderAlpha;
@@ -73,6 +74,7 @@ public class TransparencyPornFragment extends Fragment {
         private SeekBarPreferenceCham mQSCornerRadius;
         private SeekBarPreferenceCham mQSDashWidth;
         private SeekBarPreferenceCham mQSDashGap;
+        private SeekBarPreferenceCham mNotificationsAlpha;
 
         static final int DEFAULT_VOLUME_DIALOG_STROKE_COLOR = 0xFF80CBC4;
         static final int DEFAULT_QS_STROKE_COLOR = 0xFF80CBC4;
@@ -239,6 +241,14 @@ public class TransparencyPornFragment extends Fragment {
             mQSDashGap.setValue(qSDialogDashGap / 1);
             mQSDashGap.setOnPreferenceChangeListener(this);
 
+            // Notifications alpha
+            mNotificationsAlpha =
+                    (SeekBarPreferenceCham) prefSet.findPreference(PREF_NOTIFICATION_ALPHA);
+            int notificationsAlpha = Settings.System.getInt(resolver,
+                    Settings.System.NOTIFICATION_ALPHA, 255);
+            mNotificationsAlpha.setValue(notificationsAlpha / 1);
+            mNotificationsAlpha.setOnPreferenceChangeListener(this);
+
             VolumeDialogSettingsDisabler(volumeDialogStroke);
             QSSettingsDisabler(qSStroke);
 
@@ -343,6 +353,11 @@ public class TransparencyPornFragment extends Fragment {
                 int val = (Integer) newValue;
                 Settings.System.putInt(resolver,
                         Settings.System.QS_STROKE_DASH_GAP, val * 1);
+                return true;
+            } else if (preference == mNotificationsAlpha) {
+                int alpha = (Integer) newValue;
+                Settings.System.putInt(resolver,
+                        Settings.System.NOTIFICATION_ALPHA, alpha * 1);
                 return true;
             }
             return false;
