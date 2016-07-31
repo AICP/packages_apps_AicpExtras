8e6048e2f6597c8a8ada87aa9ed425b49deefd07
Raienryu97 <cherukupallip@gmail.com>
2016-07-31 21:00:08 +0530

    [WIP] Attempt to add a settings part to AE
    Change-Id: I5394849a2d80b0fcc76710168e397830ccc4ccd7

---
+++ src/com/lordclockan/aicpextras/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lordclockan.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.xml.settings_layout);
+     }
+}
+
