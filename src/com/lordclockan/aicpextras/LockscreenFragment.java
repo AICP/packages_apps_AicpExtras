056e9dbe78b9f8454974ebaeaf51b9bf6d738673
beanstown106 <nbenis106@gmail.com>
2016-10-01 20:53:14 +0200

    Longpress power lockscreen torch [2/2]
    AICPfy:
    - We already have strings from MM time
    
    Change-Id: I10c8aa06c81ca020967d5d91e169a4531ed8a06a

---
+++ src/com/lordclockan/aicpextras/LockscreenFragment.java
@@ -0,0 +1,81 @@
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.WallpaperManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import com.android.internal.util.aicp.AicpUtils;
+import com.lordclockan.R;
+import com.lordclockan.aicpextras.widget.SeekBarPreferenceCham;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class LockscreenFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new SettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class SettingsPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        public SettingsPreferenceFragment() {
+        }
+
+        private static final String PREF_KEYGUARD_TORCH = "keyguard_toggle_torch";
+
+        private SwitchPreference mKeyguardTorch;
+
+        static final int DEFAULT = 0xffffffff;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.lockscreen_layout);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+            // Keyguard Torch
+            mKeyguardTorch = (SwitchPreference) prefSet.findPreference(PREF_KEYGUARD_TORCH);
+            if (!AicpUtils.deviceSupportsFlashLight(getActivity())) {
+                prefSet.removePreference(mKeyguardTorch);
+            }
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+    }
+}
