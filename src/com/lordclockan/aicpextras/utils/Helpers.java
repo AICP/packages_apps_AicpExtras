f05b9d33be4706a79380baeb95f1d860e3fe8e6c
LorDClockaN <lordclockan@gmail.com>
2016-10-07 16:27:20 +0200

    Add SystemUI restart prompt dialog to Helpers class
    Change-Id: Ie4a26cfe23b1680752433692dae5de4e5f8030f3

--- src/com/lordclockan/aicpextras/utils/Helpers.java
+++ src/com/lordclockan/aicpextras/utils/Helpers.java
@@ -1,10 +1,16 @@
 package com.lordclockan.aicpextras.utils;
 
-import android.content.Context;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.util.Log;
 import android.widget.Toast;
@@ -17,6 +23,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 
+import com.lordclockan.R;
+
 public class Helpers {
     // avoids hardcoding the tag
     private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
@@ -27,6 +35,44 @@ public class Helpers {
 
     public static void restartSystemUI() {
         CMDProcessor.startSuCommand("pkill -f com.android.systemui");
+    }
+
+    public static void showSystemUIrestartDialog(Activity a) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(a);
+        builder.setTitle(R.string.systemui_restart_title);
+        builder.setMessage(R.string.systemui_restart_message);
+        builder.setPositiveButton(R.string.print_restart,
+                new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected void onPreExecute() {
+                        ProgressDialog dialog = new ProgressDialog(a);
+                        dialog.setMessage(a.getResources().getString(R.string.restarting_ui));
+                        dialog.setCancelable(false);
+                        dialog.setIndeterminate(true);
+                        dialog.show();
+                    }
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // Give the user a second to see the dialog
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            // Ignore
+                        }
+
+                        // Restart the UI
+                        CMDProcessor.startSuCommand("pkill -f com.android.systemui");
+                        a.finish();
+                        return null;
+                    }
+                };
+                task.execute();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.show();
     }
 
     public static boolean isPackageInstalled(String packageName, PackageManager pm) {
