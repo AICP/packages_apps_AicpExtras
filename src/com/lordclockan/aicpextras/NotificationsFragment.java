a6486386f00169c86cc1184b2c2ec0f7755139ea
xyyx <xyyx@mail.ru>
2016-10-14 14:31:58 +0200

    Custom small QS tiles [2/2]
    Change-Id: I42d7df7a3f8a3ddc562fdf32caf27402c1b5aba5

--- src/com/lordclockan/aicpextras/NotificationsFragment.java
+++ src/com/lordclockan/aicpextras/NotificationsFragment.java
@@ -55,6 +55,7 @@ public class NotificationsFragment extends Fragment {
         private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
         private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
         private static final String PREF_COLUMNS = "qs_columns";
+        private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
         private ListPreference mTileAnimationStyle;
         private ListPreference mTileAnimationDuration;
@@ -62,6 +63,7 @@ public class NotificationsFragment extends Fragment {
         private ListPreference mRowsPortrait;
         private ListPreference mRowsLandscape;
         private ListPreference mQsColumns;
+        private ListPreference mSysuiQqsCount;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -123,6 +125,13 @@ public class NotificationsFragment extends Fragment {
             mQsColumns.setSummary(mQsColumns.getEntry());
             mQsColumns.setOnPreferenceChangeListener(this);
 
+            mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+            int SysuiQqsCount = Settings.Secure.getInt(resolver,
+                   Settings.Secure.QQS_COUNT, 5);
+            mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+            mSysuiQqsCount.setOnPreferenceChangeListener(this);
+
         }
 
         @Override
@@ -175,6 +184,13 @@ public class NotificationsFragment extends Fragment {
                         Settings.Secure.QS_COLUMNS, intValue);
                 preference.setSummary(mQsColumns.getEntries()[index]);
                 return true;
+            } else if (preference == mSysuiQqsCount) {
+                String SysuiQqsCount = (String) newValue;
+                int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+                Settings.Secure.putInt(getContentResolver(), Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+                int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+                mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            return true;
             }
             return false;
         }
