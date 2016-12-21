cac26ac1db45f8bac21cfa300d8a53bb5d693867
Roman Birg <roman@cyngn.com>
2016-12-21 06:11:04 +0300

    ae: update community metrics
    - Send community metrics to AICP server
    
    - Kill off Google Analytics
    
    - Use JobScheduler to intelligently schedule uploads when network
      connection is available
    
    Adapted to AICP P by @eyosen
    - redirected to read from a system prop
    - removed redundant cyanogen.com reporting
    - added our specific report data
    - remove ga related codes from Utilities
    - prompt user for initial stats reporting opt-in
    - make reporting default value follow reporting mode
    - replace checkboxes with switches
    - add notifications channel
    - add our notifications logo
    - stick with key-value pairs when sending
    
    Change-Id: Ibd7d84a961dda4af20db9226191380e9f977b5de
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/aicp/extras/romstats/AnonymousStats.java
+++ src/com/aicp/extras/romstats/AnonymousStats.java
@@ -36,11 +36,12 @@ import android.content.pm.Signature;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceClickListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.util.ArraySet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -48,6 +49,8 @@ import android.view.MenuItem;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+
+import java.util.Set;
 
 public class AnonymousStats extends BaseSettingsFragment implements
 		DialogInterface.OnClickListener, DialogInterface.OnDismissListener,
@@ -57,8 +60,11 @@ public class AnonymousStats extends BaseSettingsFragment implements
 	private static final String PREF_LAST_REPORT_ON = "pref_last_report_on";
 	private static final String PREF_REPORT_INTERVAL = "pref_reporting_interval";
 
-	private CheckBoxPreference mEnableReporting;
-	private CheckBoxPreference mPersistentOptout;
+	/* package */ static final String KEY_JOB_QUEUE = "pref_job_queue";
+	/* package */ static final int QUEUE_MAX_THRESHOLD = 1000;
+
+	private SwitchPreference mEnableReporting;
+	private SwitchPreference mPersistentOptout;
 	private Preference mViewStats;
 
 	private Dialog mOkDialog;
@@ -83,17 +89,17 @@ public class AnonymousStats extends BaseSettingsFragment implements
 
 		PreferenceScreen prefSet = getPreferenceScreen();
 		mPrefs = getActivity().getSharedPreferences(Utilities.SETTINGS_PREF_NAME, 0);
-		mEnableReporting = (CheckBoxPreference) prefSet.findPreference(Const.ANONYMOUS_OPT_IN);
-		mPersistentOptout = (CheckBoxPreference) prefSet.findPreference(Const.ANONYMOUS_OPT_OUT_PERSIST);
+		mEnableReporting = (SwitchPreference) prefSet.findPreference(Const.ANONYMOUS_OPT_IN);
+		mPersistentOptout = (SwitchPreference) prefSet.findPreference(Const.ANONYMOUS_OPT_OUT_PERSIST);
 		mViewStats = (Preference) prefSet.findPreference(PREF_VIEW_STATS);
 
 		boolean firstBoot = mPrefs.getBoolean(Const.ANONYMOUS_FIRST_BOOT, true);
-        if (mEnableReporting.isChecked() && firstBoot) {
-        	Log.d(Const.TAG, "First app start, set params and report immediately");
-            mPrefs.edit().putBoolean(Const.ANONYMOUS_FIRST_BOOT, false).apply();
-            mPrefs.edit().putLong(Const.ANONYMOUS_LAST_CHECKED, 1).apply();
-            ReportingServiceManager.launchService(getActivity());
-        }
+		if (mEnableReporting.isChecked() && firstBoot) {
+				Log.d(Const.TAG, "First app start, set params and report immediately");
+				mPrefs.edit().putBoolean(Const.ANONYMOUS_FIRST_BOOT, false).apply();
+				mPrefs.edit().putLong(Const.ANONYMOUS_LAST_CHECKED, 1).apply();
+				ReportingServiceManager.launchService(getActivity(), false);
+		}
 
 		Preference mPrefHolder;
 		/* Experimental feature 2 */
@@ -196,9 +202,12 @@ public class AnonymousStats extends BaseSettingsFragment implements
 
 	@Override
 	public void onClick(DialogInterface dialog, int which) {
+		mPrefs.edit().putBoolean(Const.ANONYMOUS_FIRST_BOOT, false).apply();
+		Log.d(Const.TAG, "No more First Boot.");
 		if (which == DialogInterface.BUTTON_POSITIVE) {
 			mOkClicked = true;
 			mPrefs.edit().putBoolean(Const.ANONYMOUS_OPT_IN, true).apply();
+			Log.d(Const.TAG, "User opted in");
 
 			mPersistentOptout.setChecked(false);
 			try {
@@ -210,13 +219,64 @@ public class AnonymousStats extends BaseSettingsFragment implements
 			} catch (Exception e) {
 				Log.w(Const.TAG, "Unable to write persistent optout cookie", e);
 			}
-
-			ReportingServiceManager.launchService(getActivity());
+			ReportingServiceManager.launchService(getActivity(), true);
 		} else if (which == DialogInterface.BUTTON_NEGATIVE) {
 			mEnableReporting.setChecked(false);
 		} else {
 			Uri uri = Uri.parse("http://aicp-rom.com/");
 			startActivity(new Intent(Intent.ACTION_VIEW, uri));
 		}
+	}
+
+	public static Set<String> getJobQueue(Context context) {
+	    return getPreferences(context).getStringSet(KEY_JOB_QUEUE, new ArraySet<String>());
+	}
+
+	public static void clearJobQueue(Context context) {
+	    getPreferences(context)
+	            .edit()
+	            .remove(KEY_JOB_QUEUE)
+	            .commit();
+	}
+
+	public static void addJob(Context context, int jobId) {
+	    Set<String> jobQueue = getJobQueue(context);
+	    jobQueue.add(String.valueOf(jobId));
+
+	    getPreferences(context)
+	            .edit()
+	            .putStringSet(KEY_JOB_QUEUE, jobQueue)
+	            .commit();
+	}
+
+	public static void removeJob(Context context, int jobId) {
+	    Set<String> jobQueue = getJobQueue(context);
+	    jobQueue.remove(String.valueOf(jobId));
+	    getPreferences(context)
+	            .edit()
+	            .putStringSet(KEY_JOB_QUEUE, jobQueue)
+	            .commit();
+	}
+
+	/**
+	 * @param context context to use to get prefs
+	 * @return Returns the next unused int in the job queue, up until {@link #QUEUE_MAX_THRESHOLD}
+	 * is reached, then it will return -1
+	 */
+	public static int getNextJobId(Context context) {
+	    Set<String> currentQueue = getJobQueue(context);
+
+	    if (currentQueue == null) {
+	        return 1;
+	    } else if (currentQueue.size() >= QUEUE_MAX_THRESHOLD) {
+	        return -1;
+	    } else {
+	        int i = 1;
+	        while (currentQueue.contains(String.valueOf(i))) {
+	            i++;
+	        }
+	        return i;
+
+	    }
 	}
 }
---
+++ src/com/aicp/extras/romstats/StatsUploadJobService.java
@@ -0,0 +1,296 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.romstats;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.PersistableBundle;
+import android.support.v4.util.ArrayMap;
+import android.util.Log;
+import com.aicp.extras.R;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+public class StatsUploadJobService extends JobService {
+
+//    private static final String TAG = StatsUploadJobService.class.getSimpleName();
+    private static final String TAG = Const.TAG;
+    private static final boolean DEBUG = false;
+
+    public static final String KEY_JOB_TYPE = "job_type";
+    public static final int JOB_TYPE_AICP= 1;
+
+    public static final String KEY_UNIQUE_ID = "device_hash";
+    public static final String KEY_DEVICE_NAME = "device_name";
+    public static final String KEY_VERSION = "device_version";
+    public static final String KEY_BUILDTYPE = "device_buildtype";
+    public static final String KEY_COUNTRY = "device_country";
+    public static final String KEY_CARRIER = "device_carrier";
+    public static final String KEY_CARRIER_ID = "device_carrier_id";
+    public static final String KEY_ROM_NAME = "rom_name";
+    public static final String KEY_ROM_VERSION = "rom_version";
+    public static final String KEY_STATS_URL = "rom_stats_url";
+    public static final String KEY_SIGN_CERT = "sign_cert";
+    public static final String KEY_TIMESTAMP = "timeStamp";
+
+    List<JobParameters> mFinishedJobs = new LinkedList<>();
+    ArrayMap<JobParameters, StatsUploadTask> mCurrentJobs = new ArrayMap<>();
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        if (DEBUG)
+            Log.d(TAG, "onStartJob() called with " + "jobParameters = [" + jobParameters + "]");
+        final StatsUploadTask uploadTask = new StatsUploadTask(jobParameters);
+        mCurrentJobs.put(jobParameters, uploadTask);
+        uploadTask.execute((Void) null);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        if (DEBUG)
+            Log.d(TAG, "onStopJob() called with " + "jobParameters = [" + jobParameters + "]");
+
+        final StatsUploadTask cancelledJob = mCurrentJobs.remove(jobParameters);
+
+        // if we can't remove from finished jobs, that means it's not finished!
+        final boolean jobSuccessfullyFinished = mFinishedJobs.remove(jobParameters);
+
+        if (!jobSuccessfullyFinished) {
+            // cancel the ongoing background task
+            if (cancelledJob != null) {
+                cancelledJob.cancel(true);
+            }
+        }
+
+        return !jobSuccessfullyFinished;
+    }
+
+    private class StatsUploadTask extends AsyncTask<Void, Void, Boolean> {
+
+        private JobParameters mJobParams;
+
+        public StatsUploadTask(JobParameters jobParams) {
+            this.mJobParams = jobParams;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            PersistableBundle extras = mJobParams.getExtras();
+
+            String deviceId = extras.getString(KEY_UNIQUE_ID);
+            String deviceName = extras.getString(KEY_DEVICE_NAME);
+            String deviceVersion = extras.getString(KEY_VERSION);
+            String deviceBuildType = extras.getString(KEY_BUILDTYPE);
+            String deviceCountry = extras.getString(KEY_COUNTRY);
+            String deviceCarrier = extras.getString(KEY_CARRIER);
+            String deviceCarrierId = extras.getString(KEY_CARRIER_ID);
+            String romName = extras.getString(KEY_ROM_NAME);
+            String romVersion = extras.getString(KEY_ROM_VERSION);
+            String romStatsUrl = extras.getString(KEY_STATS_URL);
+            String romStatsSignCert = extras.getString(KEY_SIGN_CERT);
+            //long timeStamp = extras.getLong(KEY_TIMESTAMP);
+
+            boolean success = false;
+            if (!isCancelled()) {
+                int jobType = extras.getInt(KEY_JOB_TYPE, -1);
+
+                switch (jobType) {
+                    case JOB_TYPE_AICP:
+                        try {
+                            success = uploadToAicp(deviceId, deviceName, deviceVersion,
+                                    deviceBuildType, deviceCountry, deviceCarrier,
+                                    deviceCarrierId, romName, romVersion,
+                                    romStatsSignCert, romStatsUrl);
+                        } catch (IOException e) {
+                            Log.e(TAG, "Could not upload stats checkin to aicp server", e);
+                            success = false;
+                        }
+                        break;
+                }
+            }
+
+            if (success) {
+                AnonymousStats.removeJob(StatsUploadJobService.this, mJobParams.getJobId());
+            }
+
+            return success;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                mFinishedJobs.add(mJobParams);
+            }
+            if (DEBUG)
+                Log.d(TAG, "job id " + mJobParams.getJobId() + ", has finished with success="
+                        + success);
+            jobFinished(mJobParams, !success);
+        }
+    }
+
+
+    private boolean uploadToAicp(String deviceId, String deviceName, String deviceVersion,
+                               String deviceBuildType, String deviceCountry, String deviceCarrier,
+                               String deviceCarrierId, String romName, String romVersion,
+                               String romStatsSignCert, String romStatsUrl)
+            throws IOException {
+
+            HttpClient httpClient;
+            if (Const.SKIP_CERTIFICATE_CHECK) {
+                try {
+                    KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                    trustStore.load(null, null);
+
+                    SSLSocketFactory socketFactory = new InsecureSSLSocketFactory(trustStore);
+                    socketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+                    HttpParams params = new BasicHttpParams();
+                    HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+                    HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+                    SchemeRegistry schReg = new SchemeRegistry();
+                    schReg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+                    schReg.register(new Scheme("https", socketFactory, 443));
+
+                    ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, schReg);
+
+                    httpClient = new DefaultHttpClient(ccm, params);
+                } catch (KeyStoreException|IOException|NoSuchAlgorithmException
+                        |CertificateException|KeyManagementException|UnrecoverableKeyException e) {
+                    Log.w(Const.TAG, "Could not set up custom truststore", e);
+                    httpClient = new DefaultHttpClient();
+                }
+            } else {
+                httpClient = new DefaultHttpClient();
+            }
+            HttpPost httpPost = new HttpPost(romStatsUrl + "submit");
+            boolean success = false;
+
+            try {
+                List<NameValuePair> kv = new ArrayList<NameValuePair>(5);
+                kv.add(new BasicNameValuePair("device_hash", deviceId));
+                kv.add(new BasicNameValuePair("device_name", deviceName));
+                kv.add(new BasicNameValuePair("device_version", deviceVersion));
+                kv.add(new BasicNameValuePair("device_buildtype", deviceBuildType));
+                kv.add(new BasicNameValuePair("device_country", deviceCountry));
+                kv.add(new BasicNameValuePair("device_carrier", deviceCarrier));
+                kv.add(new BasicNameValuePair("device_carrier_id", deviceCarrierId));
+                kv.add(new BasicNameValuePair("rom_name", romName));
+                kv.add(new BasicNameValuePair("rom_version", romVersion));
+                kv.add(new BasicNameValuePair("sign_cert", romStatsSignCert));
+
+                httpPost.setEntity(new UrlEncodedFormEntity(kv));
+                HttpResponse response = httpClient.execute(httpPost);
+
+                if (DEBUG) Log.d(Const.TAG, "RESULT: code=" + response.getStatusLine().getStatusCode());
+                if (DEBUG) Log.d(Const.TAG, "RESULT: message=" + EntityUtils.toString(response.getEntity()));
+
+                success = true;
+            } catch (IOException e) {
+                Log.w(Const.TAG, "Could not upload stats checkin", e);
+            }
+            return success;
+    }
+
+    private class InsecureSSLSocketFactory extends SSLSocketFactory{
+        private SSLContext sslContext = SSLContext.getInstance("TLS");
+        public InsecureSSLSocketFactory(KeyStore trustStore) throws NoSuchAlgorithmException,
+                KeyManagementException, KeyStoreException, UnrecoverableKeyException{
+            super(trustStore);
+
+            TrustManager trustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+
+            sslContext.init(null, new TrustManager[]{trustManager}, null);
+        }
+
+        @Override
+        public Socket createSocket(Socket socket, String host, int port, boolean autoClose)
+                throws IOException{
+            return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+        }
+
+        @Override
+        public Socket createSocket() throws IOException{
+            return sslContext.getSocketFactory().createSocket();
+        }
+    }
+}
