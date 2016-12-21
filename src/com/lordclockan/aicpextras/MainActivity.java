d2f4674fd8fe0b7e25c40e3ac063757a9b7fbdde
LorDClockaN <lordclockan@gmail.com>
2016-12-21 14:34:06 +0100

    Add back Recents fragment
    Change-Id: Idc9a1ae06f05a3c0dd658057121de49e63440957

--- src/com/lordclockan/aicpextras/MainActivity.java
+++ src/com/lordclockan/aicpextras/MainActivity.java
@@ -268,6 +268,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_notif_drawer:
                 fragmentClass = NotificationsFragment.class;
                 break;
+            case R.id.nav_recents:
+                fragmentClass = RecentsPanelFragment.class;
+                break;
             case R.id.nav_lockscreen:
                 fragmentClass = LockscreenFragment.class;
                 break;
