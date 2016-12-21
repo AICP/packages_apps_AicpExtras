e2a61e587031e5f1fc4c0294bec97b1eebf752ac
Pranav Vashi <neobuddy89@gmail.com>
2016-12-21 14:38:52 +0100

    Immersive Recents [2/2]
    Change-Id: I952ce01c0e1b3f5df5414919ccd19c1cd02c4714
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/lordclockan/aicpextras/RecentsPanelFragment.java
+++ src/com/lordclockan/aicpextras/RecentsPanelFragment.java
@@ -39,6 +39,10 @@ public class RecentsPanelFragment extends Fragment {
         public RecentsPanelPreferenceFragment() {
         }
 
+        private static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        private ListPreference mImmersiveRecents;
+
         ViewGroup viewGroup;
 
         @Override
@@ -54,11 +58,25 @@ public class RecentsPanelFragment extends Fragment {
             PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
 
+            // Immersive recents
+            mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
+            mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
+                    resolver, Settings.System.IMMERSIVE_RECENTS, 0)));
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            mImmersiveRecents.setOnPreferenceChangeListener(this);
+
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mImmersiveRecents) {
+                Settings.System.putInt(resolver, Settings.System.IMMERSIVE_RECENTS,
+                        Integer.valueOf((String) newValue));
+                mImmersiveRecents.setValue(String.valueOf(newValue));
+                mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+                return true;
+            }
             return false;
         }
 
