59ce22ec3e3b8176da877eec6a3505bdf36e1c88
LorDClockaN <lordclockan@gmail.com>
2016-12-21 14:34:06 +0100

    Add back Recents fragment
    Change-Id: Idc9a1ae06f05a3c0dd658057121de49e63440957

---
+++ src/com/lordclockan/aicpextras/RecentsPanelFragment.java
@@ -0,0 +1,70 @@
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lordclockan.R;
+
+public class RecentsPanelFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new RecentsPanelPreferenceFragment())
+                .commit();
+    }
+
+    public static class RecentsPanelPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        public RecentsPanelPreferenceFragment() {
+        }
+
+        ViewGroup viewGroup;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.recents_panel_layout);
+
+            viewGroup = (ViewGroup) ((ViewGroup) getActivity()
+                    .findViewById(android.R.id.content)).getChildAt(0);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+    }
+}
