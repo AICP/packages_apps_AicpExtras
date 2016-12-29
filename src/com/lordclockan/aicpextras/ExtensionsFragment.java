2b4d9c3413603d07a2eafd50c72307378d2dab7a
Ali B <abittin@gmail.com>
2016-12-29 16:22:50 +0300

    AE: add System Extensions fragment
    - Add System App Remover as the first entry
    
    Change-Id: I31cf56d12c07823555a01dd322c73f45a5253764
    Signed-off-by: Ali B <abittin@gmail.com>

---
+++ src/com/lordclockan/aicpextras/ExtensionsFragment.java
@@ -0,0 +1,69 @@
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v4.app.Fragment;
+
+import com.lordclockan.R;
+
+public class ExtensionsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new SettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class SettingsPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        public SettingsPreferenceFragment() {
+        }
+
+        private static final String PREF_SYSTEMAPP_REMOVER = "system_app_remover";
+
+        private Preference mSystemappRemover;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.extensions_layout);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+            mSystemappRemover = prefSet.findPreference(PREF_SYSTEMAPP_REMOVER);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            final String key = preference.getKey();
+            ContentResolver resolver = getActivity().getContentResolver();
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            if (preference == mSystemappRemover) {
+                Intent intent = new Intent(getActivity(), SystemappRemover.class);
+                startActivity(intent);
+            } else {
+                return super.onPreferenceTreeClick(preferenceScreen, preference);
+            }
+            return false;
+        }
+    }
+}
