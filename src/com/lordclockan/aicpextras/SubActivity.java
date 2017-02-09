98801eb91295bed5a723124bf5746f8f061e1149
SpiritCroc <spiritcroc@gmail.com>
2017-02-09 11:25:40 +0100

    Slim recents app sidebar [2/2]
    Based on the app sidebar by 0xD34D <clark@scheffsblend.com>.
    
    Changes to the original sidebar include:
    - No trigger areas, but show/hide with the SlimRecent screen
    - Adapted look and animation to SlimRecent
    - Longpress actions
    - Slimer settings using ActionListViewSettings, also added possibility to configure sidebar size
      (roughly, as the size is adapted to the screen height in order to show full icons) and colors
    - Removed possibility to add folders to the sidebar
    - Queue apps to launch on recents close by swiping icons left/right
    
    Bring back ActionListViewSettings we had in mm, thanks to Lars Greiss <kufikugel@googlemail.com> for those.
    
    TODO Fix custom icon setting
    
    Change-Id: Ib70d5caea1c7b3b5be84a314ec504bd8798fc52a

--- src/com/lordclockan/aicpextras/SubActivity.java
+++ src/com/lordclockan/aicpextras/SubActivity.java
@@ -2,10 +2,17 @@ package com.lordclockan.aicpextras;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class SubActivity extends Activity {
+
+    public static final String EXTRA_TITLE =
+            "com.lordclockan.aicpextras.SubActivity.title";
+
+    public static final String EXTRA_FRAGMENT_CLASS =
+            "com.lordclockan.aicpextras.SubActivity.fragment_class";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,23 @@ public class SubActivity extends Activity {
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        String fragmentExtra = getIntent().getStringExtra(EXTRA_FRAGMENT_CLASS);
+        if (fragmentExtra != null  && !fragmentExtra.isEmpty()) {
+            try {
+                Class<?> fragmentClass = Class.forName(fragmentExtra);
+                Fragment fragment = (Fragment) fragmentClass.newInstance();
+                getFragmentManager().beginTransaction()
+                        .replace(android.R.id.content, fragment).commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String title = getIntent().getStringExtra(EXTRA_TITLE);
+        if (title != null  && !title.isEmpty()) {
+            setTitle(title);
         }
     }
 
