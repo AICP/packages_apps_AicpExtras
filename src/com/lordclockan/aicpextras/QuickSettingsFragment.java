47111875d2a4bd640d2ee674d086ccf8022cc799
LorDClockaN <lordclockan@gmail.com>
2017-03-12 19:26:28 +0100

    Remove tile weather icon pack settings
    Change-Id: Ic3938d6848f977d732ba4374d884a252a01244c5

--- src/com/lordclockan/aicpextras/QuickSettingsFragment.java
+++ src/com/lordclockan/aicpextras/QuickSettingsFragment.java
@@ -69,10 +69,7 @@ public class QuickSettingsFragment extends Fragment {
         private static final String PREF_COLUMNS_LANDSCAPE = "qs_columns_landscape";
         private static final String PREF_QS_DATA_ADVANCED = "qs_data_advanced";
         private static final String CATEGORY_WEATHER = "weather_category";
-        private static final String WEATHER_ICON_PACK = "weather_icon_pack";
-        private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
         private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
-        private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
 
         private ListPreference mTileAnimationStyle;
         private ListPreference mTileAnimationDuration;
@@ -83,8 +80,6 @@ public class QuickSettingsFragment extends Fragment {
         private SeekBarPreferenceCham mQsColumnsLandscape;
         private SwitchPreference mQsDataAdvanced;
         private PreferenceCategory mWeatherCategory;
-        private ListPreference mWeatherIconPack;
-        private String mWeatherIconPackNote;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -96,7 +91,6 @@ public class QuickSettingsFragment extends Fragment {
             Activity activity = getActivity();
             final ContentResolver resolver = getActivity().getContentResolver();
             final PackageManager pm = getActivity().getPackageManager();
-            mWeatherIconPackNote = getResources().getString(R.string.weather_icon_pack_note);
 
             int defaultValue;
 
@@ -159,31 +153,6 @@ public class QuickSettingsFragment extends Fragment {
             mWeatherCategory = (PreferenceCategory) prefSet.findPreference(CATEGORY_WEATHER);
             if (mWeatherCategory != null && (!Helpers.isPackageInstalled(WEATHER_SERVICE_PACKAGE, pm))) {
                 prefSet.removePreference(mWeatherCategory);
-            } else {
-                String settingsJaws = Settings.System.getString(resolver,
-                        Settings.System.OMNIJAWS_WEATHER_ICON_PACK);
-                if (settingsJaws == null) {
-                    settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
-                }
-                mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
-
-                List<String> entries = new ArrayList<String>();
-                List<String> values = new ArrayList<String>();
-                getAvailableWeatherIconPacks(entries, values);
-                mWeatherIconPack.setEntries(entries.toArray(new String[entries.size()]));
-                mWeatherIconPack.setEntryValues(values.toArray(new String[values.size()]));
-
-                int valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
-                if (valueJawsIndex == -1) {
-                    // no longer found
-                    settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
-                    Settings.System.putString(resolver,
-                            Settings.System.OMNIJAWS_WEATHER_ICON_PACK, settingsJaws);
-                    valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
-                }
-                mWeatherIconPack.setValueIndex(valueJawsIndex >= 0 ? valueJawsIndex : 0);
-                mWeatherIconPack.setSummary(mWeatherIconPackNote + "\n\n" + mWeatherIconPack.getEntry());
-                mWeatherIconPack.setOnPreferenceChangeListener(this);
             }
         }
 
@@ -236,13 +205,6 @@ public class QuickSettingsFragment extends Fragment {
                 Settings.Secure.putInt(resolver,
                         Settings.Secure.QS_COLUMNS_LANDSCAPE, intValue);
                 return true;
-            } else if (preference == mWeatherIconPack) {
-              String value = (String) newValue;
-              Settings.System.putString(resolver,
-                      Settings.System.OMNIJAWS_WEATHER_ICON_PACK, value);
-              int valueIndex = mWeatherIconPack.findIndexOfValue(value);
-              mWeatherIconPack.setSummary(mWeatherIconPackNote + " \n\n" + mWeatherIconPack.getEntries()[valueIndex]);
-              return true;
             }
             return false;
 
@@ -276,41 +238,6 @@ public class QuickSettingsFragment extends Fragment {
                     mTileAnimationDuration.setSelectable(true);
                     mTileAnimationInterpolator.setSelectable(true);
                 }
-            }
-        }
-
-        private void getAvailableWeatherIconPacks(List<String> entries, List<String> values) {
-            Intent i = new Intent();
-            PackageManager packageManager = getActivity().getPackageManager();
-            i.setAction("org.omnirom.WeatherIconPack");
-            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-                String packageName = r.activityInfo.packageName;
-                Log.d("IconPack package name: ", packageName);
-                if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
-                    values.add(0, r.activityInfo.name);
-                } else {
-                    values.add(r.activityInfo.name);
-                }
-                String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
-                if (label == null) {
-                    label = r.activityInfo.packageName;
-                }
-                if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
-                    entries.add(0, label);
-                } else {
-                    entries.add(label);
-                }
-            }
-            i = new Intent(Intent.ACTION_MAIN);
-            i.addCategory(CHRONUS_ICON_PACK_INTENT);
-            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-                String packageName = r.activityInfo.packageName;
-                values.add(packageName + ".weather");
-                String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
-                if (label == null) {
-                    label = r.activityInfo.packageName;
-                }
-                entries.add(label);
             }
         }
     }
