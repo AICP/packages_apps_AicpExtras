6407f944ca40cd1a016b613911bcbb96f07b5b5f
ezio84 <brabus84@gmail.com>
2017-04-23 18:16:37 +0200

    Ticker: allow to show music track info on new track being played [2/2]
    Change-Id: I4ccbc74c7a9a49665b60a1a44ca8552d0b8f4213

--- src/com/lordclockan/aicpextras/Ticker.java
+++ src/com/lordclockan/aicpextras/Ticker.java
@@ -55,7 +55,7 @@ public class Ticker extends SubActivity {
         private static final String PREF_ICON_COLOR = "status_bar_ticker_icon_color";
         private static final String PREF_TICKER_RESTORE_DEFAULTS = "ticker_restore_defaults";
 
-        private SwitchPreference mShowTicker;
+        private ListPreference mShowTicker;
         private ColorPickerPreference mTextColor;
         private ColorPickerPreference mIconColor;
         private Preference mTickerDefaults;
@@ -69,10 +69,13 @@ public class Ticker extends SubActivity {
             PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
 
-            mShowTicker = (SwitchPreference) prefSet.findPreference(PREF_SHOW_TICKER);
-            mShowTicker.setChecked(Settings.System.getInt(resolver,
-                    Settings.System.STATUS_BAR_SHOW_TICKER, 0) != 0);
+            mShowTicker = (ListPreference) prefSet.findPreference(PREF_SHOW_TICKER);
             mShowTicker.setOnPreferenceChangeListener(this);
+            int tickerMode = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER,
+                    0, UserHandle.USER_CURRENT);
+            mShowTicker.setValue(String.valueOf(tickerMode));
+            mShowTicker.setSummary(mShowTicker.getEntry());
 
             mTextColor = (ColorPickerPreference) prefSet.findPreference(PREF_TEXT_COLOR);
             mTextColor.setOnPreferenceChangeListener(this);
@@ -95,10 +98,13 @@ public class Ticker extends SubActivity {
 
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             ContentResolver resolver = getActivity().getContentResolver();
-            if (preference == mShowTicker) {
-                int enabled = ((Boolean) newValue) ? 1 : 0;
-                Settings.System.putInt(resolver,
-                        Settings.System.STATUS_BAR_SHOW_TICKER, enabled);
+            if (preference.equals(mShowTicker)) {
+                int tickerMode = Integer.parseInt(((String) newValue).toString());
+                Settings.System.putIntForUser(getContentResolver(),
+                        Settings.System.STATUS_BAR_SHOW_TICKER, tickerMode,
+                        UserHandle.USER_CURRENT);
+                int index = mShowTicker.findIndexOfValue((String) newValue);
+                mShowTicker.setSummary(mShowTicker.getEntries()[index]);
                 return true;
             } else if (preference == mTextColor) {
                 String hex = ColorPickerPreference.convertToARGB(
