269c596c47c0d6f3c077d53b075af77f87be4f33
Ali B <abittin@gmail.com>
2017-05-12 12:55:43 +0300

    AicpExtras: refactor weather settings
    - give weather its own fragment
    - update strings for consistency
    
    Change-Id: Ifc1cdd42cb151d82894509aab1977cc9654eafe1

---
+++ src/com/lordclockan/aicpextras/WeatherFragment.java
@@ -0,0 +1,75 @@
+package com.lordclockan.aicpextras;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v4.app.Fragment;
+
+import com.lordclockan.R;
+
+public class WeatherFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_main, new SettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class SettingsPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        public SettingsPreferenceFragment() {
+        }
+
+        private static final String CAT_LOCKSCREEN_WEATHER = "lock_screen_weather_category";
+        private static final String CAT_HEADER_WEATHER = "header_weather_category";
+
+        private static final String PREF_LOCKSCREEN_WEATHER = "lock_screen_show_weather";
+        private static final String PREF_HEADER_WEATHER = "header_weather_enabled";
+
+        private Preference mLockscreenWeather;
+        private Preference mHeaderWeather;
+        private PreferenceCategory mLockscreenWeatherCategory;
+        private PreferenceCategory mHeaderWeatherCategory;
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.weather_layout);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+            mLockscreenWeatherCategory = (PreferenceCategory) findPreference(CAT_LOCKSCREEN_WEATHER);
+            mHeaderWeatherCategory = (PreferenceCategory) findPreference(CAT_HEADER_WEATHER);
+            mLockscreenWeather = prefSet.findPreference(PREF_LOCKSCREEN_WEATHER);
+            mHeaderWeather = prefSet.findPreference(PREF_HEADER_WEATHER);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            final String key = preference.getKey();
+            ContentResolver resolver = getActivity().getContentResolver();
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+    }
+}
