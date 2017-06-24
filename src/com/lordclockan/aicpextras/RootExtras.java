38ffef474dd8cd06fcece8e9d8c875f0416b0669
Alex Cruz <mazdarider23@gmail.com>
2017-06-24 16:13:35 +0200

    Bring selinux switch to AE root extras
    Squashed commits:
    
    commit ce05f7dc7ad333b1234fee001e54ebd3e695634e
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Thu Dec 22 18:05:55 2016 +0100
    
        Selinux Switch
    
        Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b
    
    commit 6b29cf14f8c25f8ee476d86225da3faa7c632a21
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Thu Dec 22 18:11:05 2016 +0100
    
        Selinux Switch: Show toast only for permissive on boot
    
        Change-Id: I4e0589e09904332cf0dead5fe2bc9f416caf812e
    
    commit 217411c19ac0c0ac0bfd47b15d00f3d33406fa57
    Author: Meninblack007 <sanyam.53jain@gmail.com>
    Date:   Thu Dec 22 18:57:39 2016 +0100
    
        Settings: Fix selinux switch (#601)
    
        * Cleanup whitespaces
        * Use SharedPreference to save values
    
        Change-Id: I8dd13c3d2f6a16e4633933b5d750c9db412aa8e5
        Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    
        Use our SuShell utils, handle su denied, use boolean in
        shared pref, enhance our root extras fragment for
        initialization, ...
    
    Change-Id: I2469c45a794f960102534efc97f2e0c26e812987

--- src/com/lordclockan/aicpextras/RootExtras.java
+++ src/com/lordclockan/aicpextras/RootExtras.java
@@ -1,10 +1,21 @@
 package com.lordclockan.aicpextras;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.DataOutputStream;
 
 import com.lordclockan.R;
 
@@ -17,36 +28,180 @@ public class RootExtras extends Fragment {
         getActivity().getFragmentManager().beginTransaction()
                 .replace(R.id.content_main, new SettingsPreferenceFragment())
                 .commit();
-
-        // Show a dialog to the user to inform about root requirement
-        new AlertDialog.Builder(getActivity())
-                .setMessage(R.string.root_extras_msg)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Only dismiss dialog
-                        }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Leave screen by restarting the activity - this should take us
-                            // to the initially viewed fragment when opening the activity
-                            getActivity().finish();
-                            startActivity(getActivity().getIntent());
-                        }
-                })
-                .show();
-
     }
 
-    private static class SettingsPreferenceFragment extends PreferenceFragment {
+    private class SettingsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
+        private static final String TAG = "RootExtras";
+
+        private static final String SELINUX = "selinux";
+
+        private SwitchPreference mSelinux;
+
+        private Toast mSuDeniedToast;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             addPreferencesFromResource(R.xml.root_extras);
+
+            mSuDeniedToast = Toast.makeText(getActivity().getApplicationContext(),
+                        getString(R.string.cannot_get_su_start), Toast.LENGTH_LONG);
+
+            //SELinux
+            mSelinux = (SwitchPreference) findPreference(SELINUX);
+            mSelinux.setOnPreferenceChangeListener(this);
+
+            // Show a dialog to the user to inform about root requirement
+            new AlertDialog.Builder(getActivity())
+                    .setMessage(R.string.root_extras_msg)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // We're staying here, so do the rest of the initialization that
+                                // requires root
+                                new InitPrefsTask().execute();
+                            }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                leave();
+                            }
+                    })
+                    .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                // User was to lazy to use the buttons to close the dialog,
+                                // so let's assume they know what they're doing and continue
+                                // with initialization, now the part that requires root
+                                new InitPrefsTask().execute();
+                            }
+                    })
+                    .show();
         }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (preference == mSelinux) {
+                if (newValue.toString().equals("true")) {
+                    new SwitchSelinuxTask().execute(true);
+                    setSelinuxEnabled(true);
+                } else if (newValue.toString().equals("false")) {
+                    new SwitchSelinuxTask().execute(false);
+                    setSelinuxEnabled(false);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private abstract class SuTask<Params> extends AsyncTask<Params, Void, Void> {
+
+            protected Exception mException = null;
+
+            abstract protected void sudoInBackground(Params... params)
+                    throws SuShell.SuDeniedException;
+
+            @Override
+            protected Void doInBackground(Params... params) {
+                try {
+                    sudoInBackground(params);
+                } catch (SuShell.SuDeniedException e) {
+                    mException = e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                if (mException instanceof SuShell.SuDeniedException) {
+                    mSuDeniedToast.show();
+                } else {
+                    mSuDeniedToast.cancel();
+                }
+            }
+        }
+
+        private class InitPrefsTask extends SuTask<Void> {
+
+            @Override
+            protected void sudoInBackground(Void... params) throws SuShell.SuDeniedException {
+                boolean selinuxEnforcing =
+                        SuShell.runWithSuCheck("getenforce").contains("Enforcing");
+                Log.d(TAG, "Detected selinux enforcing status: " + selinuxEnforcing);
+                getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setSelinuxStatus(selinuxEnforcing);
+                        }
+                });
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                if (mException instanceof SuShell.SuDeniedException) {
+                    // Since we cannot get root access while trying to set up the fragment, leave
+                    leave();
+                }
+            }
+        }
+
+        private void setSelinuxStatus(boolean status) {
+            mSelinux.setChecked(status);
+            if (status) {
+                mSelinux.setSummary(R.string.selinux_enforcing_title);
+            } else {
+                mSelinux.setSummary(R.string.selinux_permissive_title);
+            }
+        }
+
+        private void setSelinuxEnabled(boolean status) {
+            SharedPreferences.Editor editor =
+                    getContext().getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+            editor.putBoolean("selinux", status);
+            editor.apply();
+            setSelinuxStatus(status);
+        }
+
+        private class SwitchSelinuxTask extends SuTask<Boolean> {
+
+            @Override
+            protected void sudoInBackground(Boolean... params) {
+                if (params.length != 1) {
+                    Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+                    return;
+                }
+                try {
+                    if (params[0]) {
+                        SuShell.runWithSuCheck("setenforce 1");
+                    } else {
+                        SuShell.runWithSuCheck("setenforce 0");
+                    }
+                } catch (SuShell.SuDeniedException e) {
+                    mException = e;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                if (mException instanceof SuShell.SuDeniedException) {
+                    // Did not work, so return to previous value
+                    setSelinuxEnabled(!mSelinux.isChecked());
+                }
+            }
+        }
+    }
+
+    private void leave() {
+        // Leave screen by restarting the activity - this should take us
+        // to the initially viewed fragment when opening the activity
+        getActivity().finish();
+        startActivity(getActivity().getIntent());
     }
 }
