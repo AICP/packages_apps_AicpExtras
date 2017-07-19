642e6b22a94211f73d45275702e2a50257084e91
SpiritCroc <spiritcroc@gmail.com>
2017-07-19 10:43:15 +0200

    AE: add WakeBlock
    open app if installed, or open PlayStore page if not
    
    Change-Id: Ia839c3c647f46c69f1345e009ac7c5642b21a54c

--- src/com/lordclockan/aicpextras/ExtensionsFragment.java
+++ src/com/lordclockan/aicpextras/ExtensionsFragment.java
@@ -12,7 +12,10 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.support.v4.app.Fragment;
 
+import com.lordclockan.aicpextras.utils.Helpers;
 import com.lordclockan.R;
+
+import android.net.Uri;
 
 public class ExtensionsFragment extends Fragment {
 
@@ -34,10 +37,14 @@ public class ExtensionsFragment extends Fragment {
         private static final String PREF_APP_SIDE_BAR = "app_side_bar";
         private static final String PREF_PIE = "pa_pie_control";
         private static final String PREF_APP_CIRCLE_BAR = "app_circle_bar";
+        private static final String PREF_WAKEBLOCK_APP = "wakeblock_app";
+
+        private static final String WAKEBLOCK_APP_PACKAGE = "com.giovannibozzano.wakeblock";
 
         private Preference mAppSideBar;
         private Preference mPie;
         private Preference mAppCircleBar;
+        private Preference mWakeblockApp;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -51,7 +58,15 @@ public class ExtensionsFragment extends Fragment {
             mAppSideBar = prefSet.findPreference(PREF_APP_SIDE_BAR);
             mPie = prefSet.findPreference(PREF_PIE);
             mAppCircleBar = prefSet.findPreference(PREF_APP_CIRCLE_BAR);
+            mWakeblockApp = prefSet.findPreference(PREF_WAKEBLOCK_APP);
 
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            updateWakeblockAppSummary();
         }
 
         @Override
@@ -72,10 +87,27 @@ public class ExtensionsFragment extends Fragment {
             } else if (preference == mAppCircleBar) {
                 Intent intent = new Intent(getActivity(), AppCircleBar.class);
                 getActivity().startActivity(intent);
+            } else if (preference == mWakeblockApp) {
+                Intent intent = getActivity().getPackageManager().getLaunchIntentForPackage(
+                        WAKEBLOCK_APP_PACKAGE);
+                if (intent == null) {
+                    intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + WAKEBLOCK_APP_PACKAGE));
+                }
+                startActivity(intent);
             } else {
                 return super.onPreferenceTreeClick(preferenceScreen, preference);
             }
             return true;
         }
+
+        private void updateWakeblockAppSummary() {
+            if (Helpers.isPackageInstalled(WAKEBLOCK_APP_PACKAGE, getActivity().getPackageManager())) {
+                mWakeblockApp.setSummary(R.string.wakeblock_app_installed_summary);
+            } else {
+                mWakeblockApp.setSummary(R.string.wakeblock_app_not_installed_summary);
+            }
+        }
+
     }
 }
