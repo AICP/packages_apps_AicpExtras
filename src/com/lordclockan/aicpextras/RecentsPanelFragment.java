7133a0c5564789f8c71d790c21c75028fac4019f
SpiritCroc <spiritcroc@gmail.com>
2017-08-15 21:31:26 +0200

    [WIP][2/2] AE: featureize ro.recents.grid
    TODO: disable some settings when enabled
    
    Change-Id: I502fdde942c95d087d4739b5618419ca25b13d3e

--- src/com/lordclockan/aicpextras/RecentsPanelFragment.java
+++ src/com/lordclockan/aicpextras/RecentsPanelFragment.java
@@ -56,6 +56,7 @@ public class RecentsPanelFragment extends Fragment {
         private static final String CATEGORY_OMNI_RECENTS = "omni_recents";
         private static final String CATEGORY_SLIM_RECENTS = "slim_recents";
         private static final String PREF_HIDE_APP_FROM_RECENTS = "hide_app_from_recents";
+        private static final String PREF_GRID_RECENTS = "grid_recents";
 
         private ListPreference mImmersiveRecents;
         private ListPreference mRecentsClearAllLocation;
@@ -68,6 +69,7 @@ public class RecentsPanelFragment extends Fragment {
         private SwitchPreference mRecentsUseOmniSwitch;
         private SwitchPreference mRecentsUseSlim;
         private Preference mHideAppsFromRecents;
+        private static SwitchPreference mGridRecents;
 
         private boolean mOmniSwitchInitCalled;
 
@@ -125,6 +127,8 @@ public class RecentsPanelFragment extends Fragment {
             updateRecents();
 
             mHideAppsFromRecents = prefSet.findPreference(PREF_HIDE_APP_FROM_RECENTS);
+
+            mGridRecents = (SwitchPreference) prefSet.findPreference(PREF_GRID_RECENTS);
 
         }
 
