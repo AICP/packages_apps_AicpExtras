592b6bf86f19251b1a4a74926bd38af365877fb8
SpiritCroc <spiritcroc@gmail.com>
2017-09-17 15:25:34 +0200

    AE: Oreo rework
    - New activity/fragment structure
    
    - Use AppCompat as little as possible
    
    - Inherit DeviceDefault settings theme by default
    
    - Theme setting
    
    - Added back color picker preference
    
    - Added back SeekBarPreferenceCham
    
    - Add back stats, this time without GoogleAnalytics
    
    - Add back changelog
    
    - Introduce adaptive icon
    
    - Add SystemSettingsStore
    
        To make any preference a SystemSettingsPreference, call
        myWhateverPreference.setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
    
    - Added new SystemSettingsSwitchPreference that uses the new
      SystemSettingsStore
    
    - Add Util.requireRoot() to remove preferences when no su found
      Util.requireRoot(myPreference) after initialization to hide the
      preference on devices without su
    
    - Add back AICP log it
    
    - AICP log it share zip workaround: temporarily disable death on file
      uri exposure
    
    - Add back SELinux switch, make reading current state work rootless,
      also show toast when switching to enforcing on devices that are
      permissive by default and when su was denied
    
    - Add back custom bootanimation
        - Add own AnimationDrawable subclass so we don't need frameworks
          modifications anymore
        - Create backup dir in case it doesn't already exist
    
    - Add back system app remover
        Use mount options rw,remount instead of remount,rw to fix error
        "Device or resource busy"
    
    - Add back launcher shortcuts, change color to amber & get it from color
      resources
    
    - Add back restart systemui utils
        - Rewrite them to use classes directly, we're system app!
    
    PS2:
    - Update dashboard logo
    
    PS3:
    - Fix light theme -> dark theme switch keeping icons suited for light
      statusbar themes only
    
    PS4:
    - Fix SELinux preference in wrong sharedPreferences
    - Fix double-tapping SELinux switch before denying SU leading to wrong
      checked status
    
    PS5:
    - Make it a privileged app again
    
    Change-Id: Idd6e924ce2399dae9c02b8107c43e9e767bc8a0f

---
+++ src/com/aicp/extras/romstats/Preview.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.romstats;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceScreen;
+import android.widget.Toast;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class Preview extends BaseSettingsFragment {
+
+    private static final String UNIQUE_ID = "preview_id";
+    private static final String DEVICE = "preview_device";
+    private static final String VERSION = "preview_version";
+    private static final String BUILDTYPE = "preview_buildtype";
+    private static final String COUNTRY = "preview_country";
+    private static final String CARRIER = "preview_carrier";
+    private static final String ROMNAME = "preview_romname";
+    private static final String ROMVERSION = "preview_romversion";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		addPreferencesFromResource(R.xml.preview_data);
+
+		final PreferenceScreen prefSet = getPreferenceScreen();
+		final Context context = getActivity().getApplicationContext();
+
+        prefSet.findPreference(UNIQUE_ID).setSummary(Utilities.getUniqueID(context));
+        prefSet.findPreference(DEVICE).setSummary(Utilities.getDevice());
+        prefSet.findPreference(VERSION).setSummary(Utilities.getModVersion());
+        prefSet.findPreference(BUILDTYPE).setSummary(Utilities.getBuildType());
+        prefSet.findPreference(COUNTRY).setSummary(Utilities.getCountryCode(context));
+        prefSet.findPreference(CARRIER).setSummary(Utilities.getCarrier(context));
+        prefSet.findPreference(ROMNAME).setSummary(Utilities.getRomName());
+        prefSet.findPreference(ROMVERSION).setSummary(Utilities.getRomVersion());
+	}
+}
