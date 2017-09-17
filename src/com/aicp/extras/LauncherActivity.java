0a64c9408db7231af76dc26c53d58ec154968122
SpiritCroc <spiritcroc@gmail.com>
2017-09-17 15:25:34 +0200

    AE: Oreo rework
    - New activity/fragment structure
    
    - Use AppCompat as little as possible
    
    - Inherit DeviceDefault settings theme by default
    
    - Theme setting
    
    - Added back color picker preference
    
    - Added back SeekBarPreferenceCham
    
    - Add back stats, this time without GoogleAnalytics
    
    - Add back changelog
    
    - Introduce adaptive icon
    
    - Add SystemSettingsStore
    
        To make any preference a SystemSettingsPreference, call
        myWhateverPreference.setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
    
    - Added new SystemSettingsSwitchPreference that uses the new
      SystemSettingsStore
    
    - Add Util.requireRoot() to remove preferences when no su found
      Util.requireRoot(myPreference) after initialization to hide the
      preference on devices without su
    
    - Add back AICP log it
    
    - AICP log it share zip workaround: temporarily disable death on file
      uri exposure
    
    - Add back SELinux switch, make reading current state work rootless,
      also show toast when switching to enforcing on devices that are
      permissive by default and when su was denied
    
    - Add back custom bootanimation
        - Add own AnimationDrawable subclass so we don't need frameworks
          modifications anymore
        - Create backup dir in case it doesn't already exist
    
    - Add back system app remover
        Use mount options rw,remount instead of remount,rw to fix error
        "Device or resource busy"
    
    - Add back launcher shortcuts, change color to amber & get it from color
      resources
    
    - Add back restart systemui utils
        - Rewrite them to use classes directly, we're system app!
    
    PS2:
    - Update dashboard logo
    
    PS3:
    - Fix light theme -> dark theme switch keeping icons suited for light
      statusbar themes only
    
    Change-Id: Idd6e924ce2399dae9c02b8107c43e9e767bc8a0f

---
+++ src/com/aicp/extras/LauncherActivity.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.aicp.extras;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
+import com.aicp.extras.utils.Util;
+
+public class LauncherActivity extends SettingsActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initShortcutManager();
+    }
+
+    private void initShortcutManager() {
+        final ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+
+        if (Util.isPackageEnabled(Constants.AICP_OTA_PACKAGE, this.getPackageManager())) {
+            // Intent for launching AICP OTA
+            final Intent INTENT_OTA = new Intent().setComponent(new ComponentName(
+                    Constants.AICP_OTA_PACKAGE, Constants.AICP_OTA_ACTIVITY));
+            INTENT_OTA.setAction(Intent.ACTION_VIEW);
+
+            ShortcutInfo aicpotaShortcut = new ShortcutInfo.Builder(this, "aicpota")
+                    .setShortLabel(getString(R.string.aicp_ota_title))
+                    .setLongLabel(getString(R.string.aicp_ota_title))
+                    .setIcon(Icon.createWithResource(this,
+                            R.drawable.ic_launcher_shortcut_ota))
+                    .setIntent(INTENT_OTA)
+                    .setRank(0)
+                    .build();
+            shortcutManager.setDynamicShortcuts(Arrays.asList(aicpotaShortcut));
+        } else {
+            ShortcutInfo downloadsShortcut = new ShortcutInfo.Builder(this, "downloads")
+                    .setShortLabel(getString(R.string.aicp_downloads_title))
+                    .setLongLabel(getString(R.string.aicp_downloads_title))
+                    .setIcon(Icon.createWithResource(this,
+                            R.drawable.ic_launcher_shortcut_downloads))
+                    .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse(
+                            Util.getDownloadLinkForDevice(this))))
+                    .setRank(0)
+                    .build();
+            shortcutManager.setDynamicShortcuts(Arrays.asList(downloadsShortcut));
+        }
+    }
+}
--- src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -29,7 +29,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-import com.lordclockan.R;
+import com.aicp.extras.R;
 
 public class ColorPickerDialog
         extends
