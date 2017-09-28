ce7fc38c12490ddf41e3f4dd520da88e850fe1ac
LorDClockaN <lordclockan@gmail.com>
2017-09-28 21:49:07 +0200

    PanelPorn: Volume dialog (2/2)
    Change-Id: I41aee547283255853c4f420e577a875d7ad2cf79

--- src/com/aicp/extras/fragments/PanelPorn.java
+++ src/com/aicp/extras/fragments/PanelPorn.java
@@ -18,16 +18,65 @@
 package com.aicp.extras.fragments;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.provider.Settings;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 
-public class PanelPorn extends BaseSettingsFragment {
+public class PanelPorn extends BaseSettingsFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private ListPreference mVolumeDialogStroke;
+    private Preference mVolumeDialogStrokeColor;
+    private Preference mVolumeDialogStrokeThickness;
+    private Preference mVolumeDialogDashWidth;
+    private Preference mVolumeDialogDashGap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.panel_porn);
+
+        mVolumeDialogStroke =
+                (ListPreference) findPreference(Settings.System.VOLUME_DIALOG_STROKE);
+        mVolumeDialogStroke.setOnPreferenceChangeListener(this);
+        mVolumeDialogStrokeColor = findPreference(Settings.System.VOLUME_DIALOG_STROKE_COLOR);
+        mVolumeDialogStrokeThickness =
+                findPreference(Settings.System.VOLUME_DIALOG_STROKE_THICKNESS);
+        mVolumeDialogDashWidth = findPreference(Settings.System.VOLUME_DIALOG_STROKE_DASH_WIDTH);
+        mVolumeDialogDashGap = findPreference(Settings.System.VOLUME_DIALOG_STROKE_DASH_GAP);
+        updateVolumeDialogDependencies(mVolumeDialogStroke.getValue());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mVolumeDialogStroke) {
+            updateVolumeDialogDependencies((String) newValue);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private void updateVolumeDialogDependencies(String volumeDialogStroke) {
+        if (volumeDialogStroke.equals("0")) {
+            mVolumeDialogStrokeColor.setEnabled(false);
+            mVolumeDialogStrokeThickness.setEnabled(false);
+            mVolumeDialogDashWidth.setEnabled(false);
+            mVolumeDialogDashGap.setEnabled(false);
+        } else if (volumeDialogStroke.equals("1")) {
+            mVolumeDialogStrokeColor.setEnabled(false);
+            mVolumeDialogStrokeThickness.setEnabled(true);
+            mVolumeDialogDashWidth.setEnabled(true);
+            mVolumeDialogDashGap.setEnabled(true);
+        } else {
+            mVolumeDialogStrokeColor.setEnabled(true);
+            mVolumeDialogStrokeThickness.setEnabled(true);
+            mVolumeDialogDashWidth.setEnabled(true);
+            mVolumeDialogDashGap.setEnabled(true);
+        }
     }
 }
