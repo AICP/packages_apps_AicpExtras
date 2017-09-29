ddf1a8dcc71db99cfb510a16e191d1a8bfb6d661
SpiritCroc <spiritcroc@gmail.com>
2017-09-29 11:00:47 +0200

    SystemSettingEditTextPreference: dynamic summary when summary empty otherwise
    Change-Id: Ieb344dfa17d73a4ae10709fb72f75bc9e3a31b88

--- src/com/aicp/extras/preference/SystemSettingEditTextPreference.java
+++ src/com/aicp/extras/preference/SystemSettingEditTextPreference.java
@@ -18,9 +18,11 @@ package com.aicp.extras.preference;
 
 import android.content.Context;
 import android.preference.EditTextPreference;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 public class SystemSettingEditTextPreference extends EditTextPreference {
+    private boolean mAutoSummary = false;
 
     public SystemSettingEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -44,5 +46,23 @@ public class SystemSettingEditTextPreference extends EditTextPreference {
         //setText(restoreValue ? getPersistedString(mText) : (String) defaultValue);
         // Instead, we better do
         setText(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(text, true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
     }
 }
