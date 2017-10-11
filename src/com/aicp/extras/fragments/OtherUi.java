3c2504d58f63d7f137255d26b03dd1ce121f7d41
Jon Evans <jon@craftyjon.com>
2017-10-11 21:44:52 +0200

    Revert "Port: Wake on plug (2/2)"
    This reverts commit fc5ff5d8e7978343d44d034dfdad2697499cbb17.
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/other_ui.xml
    	src/com/aicp/extras/fragments/OtherUi.java
    
    Change-Id: I636bd24981726cbcaec74e0b2456ebc796f1b50d

--- src/com/aicp/extras/fragments/OtherUi.java
+++ src/com/aicp/extras/fragments/OtherUi.java
@@ -33,12 +33,10 @@ public class OtherUi extends BaseSettingsFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
-    private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
     private static final String KEY_VIBRATE_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
 
     private SwitchPreference mShowCpuInfo;
     private SwitchPreference mVibrateOnPlug;
-    private SwitchPreference mWakeWhenPluggedOrUnplugged;
 
     @Override
     protected int getPreferenceResource() {
@@ -54,29 +52,11 @@ public class OtherUi extends BaseSettingsFragment
                 Settings.Global.SHOW_CPU_OVERLAY, 0) == 1);
         mShowCpuInfo.setOnPreferenceChangeListener(this);
 
-        mWakeWhenPluggedOrUnplugged =
-                (SwitchPreference) findPreference(KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
-
         mVibrateOnPlug =
                 (SwitchPreference) findPreference(KEY_VIBRATE_ON_CHARGE_STATE_CHANGED);
 
         if(!Util.hasVibrator(getActivity())){
             mVibrateOnPlug.getParent().removePreference(mVibrateOnPlug);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // Default value for wake-on-plug behavior from config.xml
-        boolean wakeUpWhenPluggedOrUnpluggedConfig = getResources().getBoolean(
-                com.android.internal.R.bool.config_unplugTurnsOnScreen);
-
-        if (mWakeWhenPluggedOrUnplugged != null) {
-        mWakeWhenPluggedOrUnplugged.setChecked(Settings.Global.getInt(getActivity().getContentResolver(),
-                Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
-                (wakeUpWhenPluggedOrUnpluggedConfig ? 1 : 0)) == 1);
         }
     }
 
