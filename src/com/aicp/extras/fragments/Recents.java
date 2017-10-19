44fa30e7ada1874ccf739361bb7bd6a0975b2330
SpiritCroc <spiritcroc@gmail.com>
2017-10-19 15:11:05 +0200

    AE: Add SlimRecents [3/3]
    Change-Id: I554e8c9b2644db9c6d86cc8af4bdacac5ce35f98

--- src/com/aicp/extras/fragments/Recents.java
+++ src/com/aicp/extras/fragments/Recents.java
@@ -18,14 +18,79 @@
 package com.aicp.extras.fragments;
 
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.os.Bundle;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.extras.preference.MasterSwitchPreference;
 
 public class Recents extends BaseSettingsFragment {
+
+    private static final String PREF_STOCK_RECENTS_CATEGORY = "stock_recents_category";
+    private static final String PREF_ALTERNATIVE_RECENTS_CATEGORY = "alternative_recents_category";
+
+    private PreferenceCategory mStockRecentsCategory;
+    private PreferenceCategory mAlternativeRecentsCategory;
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.recents;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mStockRecentsCategory = (PreferenceCategory) findPreference(PREF_STOCK_RECENTS_CATEGORY);
+        mAlternativeRecentsCategory =
+                (PreferenceCategory) findPreference(PREF_ALTERNATIVE_RECENTS_CATEGORY);
+
+        // Alternative recents en-/disabling
+        Preference.OnPreferenceChangeListener alternativeRecentsChangeListener =
+                new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                updateDependencies((Boolean) newValue ? preference : null);
+                return true;
+            }
+        };
+        for (int i = 0; i < mAlternativeRecentsCategory.getPreferenceCount(); i++) {
+            Preference preference = mAlternativeRecentsCategory.getPreference(i);
+            if (preference instanceof MasterSwitchPreference) {
+                preference.setOnPreferenceChangeListener(alternativeRecentsChangeListener);
+            }
+        }
+        updateDependencies(null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        for (int i = 0; i < mAlternativeRecentsCategory.getPreferenceCount(); i++) {
+            Preference preference = mAlternativeRecentsCategory.getPreference(i);
+            if (preference instanceof MasterSwitchPreference) {
+                ((MasterSwitchPreference) preference).reloadValue();
+            }
+        }
+    }
+
+    private void updateDependencies(Preference enabledAlternativeRecentsPreference) {
+        boolean alternativeRecentsEnabled = false;
+        for (int i = 0; i < mAlternativeRecentsCategory.getPreferenceCount(); i++) {
+            Preference preference = mAlternativeRecentsCategory.getPreference(i);
+            if (enabledAlternativeRecentsPreference != null
+                    && enabledAlternativeRecentsPreference != preference
+                    && preference instanceof MasterSwitchPreference
+                    && ((MasterSwitchPreference) preference).isChecked()) {
+                // Only one alternative recents at the time!
+                ((MasterSwitchPreference) preference).setCheckedPersisting(false);
+            } else if (preference instanceof MasterSwitchPreference
+                    && ((MasterSwitchPreference) preference).isChecked()) {
+                alternativeRecentsEnabled = true;
+            }
+        }
+        mStockRecentsCategory.setEnabled(!alternativeRecentsEnabled);
     }
 }
