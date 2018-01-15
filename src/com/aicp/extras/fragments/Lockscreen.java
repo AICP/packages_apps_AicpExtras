4fcdd5e8c9f308a49965195b040003aa194a558e
Miccia <bono.michele94@gmail.com>
2018-01-15 12:50:49 +0100

    AE: Remove FP preferences on null object
    On some devices (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE)
    is returning NULL.
    Avoid NullPointerException on if statement
    
    Change-Id: I22a43f2a58ec1b6aee59530530fa4e12eb14e4d4

--- src/com/aicp/extras/fragments/Lockscreen.java
+++ src/com/aicp/extras/fragments/Lockscreen.java
@@ -51,12 +51,12 @@ public class Lockscreen extends BaseSettingsFragment {
         // Fingerprint vibration
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SwitchPreference) prefSet.findPreference(FP_SUCCESS_VIBRATION);
-        if (!mFingerprintManager.isHardwareDetected()){
+        if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()){
             prefSet.removePreference(mFingerprintVib);
         }
         // Fingerprint unlock keystore
         mFpKeystore = (SwitchPreference) prefSet.findPreference(FP_UNLOCK_KEYSTORE);
-        if (!mFingerprintManager.isHardwareDetected()){
+        if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()){
             prefSet.removePreference(mFpKeystore);
         }
     }
