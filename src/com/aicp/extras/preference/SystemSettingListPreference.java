2940cd0ff6744d9f0f223f9062e864ba606e4ecd
SpiritCroc <spiritcroc@gmail.com>
2018-02-04 21:58:44 +0100

    Add non-system ListPreference with auto-summary
    Use for pulse mode
    
    Change-Id: I7f84d330fd97bac6c0954ed52d50d67e7077a7df

--- src/com/aicp/extras/preference/SystemSettingListPreference.java
+++ src/com/aicp/extras/preference/SystemSettingListPreference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 AICP
+ * Copyright (C) 2017-2018 AICP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,12 +17,10 @@
 package com.aicp.extras.preference;
 
 import android.content.Context;
-import android.support.v7.preference.ListPreference;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 
 public class SystemSettingListPreference extends ListPreference {
-    private boolean mAutoSummary = false;
 
     public SystemSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -37,24 +35,6 @@ public class SystemSettingListPreference extends ListPreference {
     public SystemSettingListPreference(Context context) {
         super(context);
         setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
-    }
-
-    @Override
-    public void setValue(String value) {
-        super.setValue(value);
-        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
-            setSummary(getEntry(), true);
-        }
-    }
-
-    @Override
-    public void setSummary(CharSequence summary) {
-        setSummary(summary, false);
-    }
-
-    private void setSummary(CharSequence summary, boolean autoSummary) {
-        mAutoSummary = autoSummary;
-        super.setSummary(summary);
     }
 
     @Override
