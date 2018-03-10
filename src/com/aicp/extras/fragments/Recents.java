da84ce1d85d6466f6fcea1db149b54716193d852
maxwen <max.weninger@gmail.com>
2018-03-10 05:10:16 +0000

    [2/2] ae: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    Change-Id: Ia21792972dd7913d5c3f647d95ac79edb00096dd

--- src/com/aicp/extras/fragments/Recents.java
+++ src/com/aicp/extras/fragments/Recents.java
@@ -55,11 +55,13 @@ public class Recents extends BaseSettingsFragment implements OnPreferenceChangeL
     private static final String PREF_ALTERNATIVE_RECENTS_CATEGORY = "alternative_recents_category";
     private static final String PREF_SWIPE_UP_ENABLED = "swipe_up_enabled_warning";
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_TYPE = "recents_layout_style";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
     private ListPreference mRecentsComponentType;
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
+    private ListPreference mRecentsType;
 
     private PreferenceCategory mStockRecentsCategory;
     private PreferenceCategory mAlternativeRecentsCategory;
@@ -91,6 +93,14 @@ public class Recents extends BaseSettingsFragment implements OnPreferenceChangeL
         mRecentsClearAllLocation.setValue(String.valueOf(location));
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
+        // oreo recents type
+        mRecentsType = (ListPreference) findPreference(RECENTS_TYPE);
+        int style = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_LAYOUT_STYLE, 0, UserHandle.USER_CURRENT);
+        mRecentsType.setValue(String.valueOf(style));
+        mRecentsType.setSummary(mRecentsType.getEntry());
+        mRecentsType.setOnPreferenceChangeListener(this);
 
         mStockRecentsCategory = (PreferenceCategory) findPreference(PREF_STOCK_RECENTS_CATEGORY);
         mAlternativeRecentsCategory =
@@ -184,6 +194,14 @@ public class Recents extends BaseSettingsFragment implements OnPreferenceChangeL
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+        return true;
+        } else if (preference == mRecentsType) {
+            int style = Integer.valueOf((String) objValue);
+            int index = mRecentsType.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, style, UserHandle.USER_CURRENT);
+            mRecentsType.setSummary(mRecentsType.getEntries()[index]);
+           Util.showSystemUiRestartDialog(getContext());
         return true;
         }
         return false;
