e6895bed47b015e9bdbd52132062b08c82b8ed34
Michele Bono <bono.michele94@gmail.com>
2018-05-04 10:29:34 +0200

    AE: FP sensor at screen on only
    Someone (like me) do not like when fingerprint unlock take you directly to desktop, add an option to make fp sensor work only when screen is on
    
    Change-Id: I74cf75101cb86e3129b5a51fcde40fbae1deca39

--- src/com/aicp/extras/fragments/Lockscreen.java
+++ src/com/aicp/extras/fragments/Lockscreen.java
@@ -32,10 +32,12 @@ public class Lockscreen extends BaseSettingsFragment {
 
     private static final String FP_SUCCESS_VIBRATION = "fingerprint_success_vib";
     private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+    private static final String FP_ONLY_SCREEN_ON = "fp_only_screen_on";
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintVib;
     private SwitchPreference mFpKeystore;
+    private SwitchPreference mFpOnlyScreenOn;
 
     @Override
     protected int getPreferenceResource() {
@@ -60,10 +62,13 @@ public class Lockscreen extends BaseSettingsFragment {
         mFingerprintVib = (SwitchPreference) prefSet.findPreference(FP_SUCCESS_VIBRATION);
         // Fingerprint unlock keystore
         mFpKeystore = (SwitchPreference) prefSet.findPreference(FP_UNLOCK_KEYSTORE);
+        // Fingerprint only at screen on
+        mFpOnlyScreenOn = (SwitchPreference) prefSet.findPreference(FP_ONLY_SCREEN_ON);
 
         if (mFingerprintManager == null && !mFingerprintManager.isHardwareDetected()){
             mFingerprintVib.getParent().removePreference(mFingerprintVib);
             mFpKeystore.getParent().removePreference(mFpKeystore);
+            mFpOnlyScreenOn.getParent().removePreference(mFpOnlyScreenOn);
         }
     }
 }
