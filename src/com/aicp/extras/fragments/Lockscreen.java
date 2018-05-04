35d2884089d450cc9247769b6f6b4a42056dec29
Michele Bono <bono.michele94@gmail.com>
2018-05-04 09:59:24 +0200

    AE: reverting wake-and-unlock to stock lineage behavior
    Change-Id: Iaa33c9e41982971f7ea035e2e7c3c6912179eb8f

--- src/com/aicp/extras/fragments/Lockscreen.java
+++ src/com/aicp/extras/fragments/Lockscreen.java
@@ -32,12 +32,10 @@ public class Lockscreen extends BaseSettingsFragment {
 
     private static final String FP_SUCCESS_VIBRATION = "fingerprint_success_vib";
     private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
-    private static final String FP_WAKE_UNLOCK = "fp_wake_and_unlock";
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintVib;
     private SwitchPreference mFpKeystore;
-    private SwitchPreference mFpWakeAndUnlock;
 
     @Override
     protected int getPreferenceResource() {
@@ -50,9 +48,6 @@ public class Lockscreen extends BaseSettingsFragment {
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
-        boolean configWakeAndUnlockEnabled = getActivity().getResources().getBoolean(
-                com.android.internal.R.bool.config_fingerprintWakeAndUnlock);
-
         try {
             mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         } catch (Exception e) {
@@ -62,17 +57,10 @@ public class Lockscreen extends BaseSettingsFragment {
         mFingerprintVib = (SwitchPreference) prefSet.findPreference(FP_SUCCESS_VIBRATION);
         // Fingerprint unlock keystore
         mFpKeystore = (SwitchPreference) prefSet.findPreference(FP_UNLOCK_KEYSTORE);
-        // Fingerprint wake and unlock
-        mFpWakeAndUnlock = (SwitchPreference) prefSet.findPreference(FP_WAKE_UNLOCK);
 
-        if (mFingerprintManager != null && mFingerprintManager.isHardwareDetected()){
-            if (configWakeAndUnlockEnabled){
-                mFpWakeAndUnlock.getParent().removePreference(mFpWakeAndUnlock);
-            }
-        } else {
+        if (mFingerprintManager == null && !mFingerprintManager.isHardwareDetected()){
             mFingerprintVib.getParent().removePreference(mFingerprintVib);
             mFpKeystore.getParent().removePreference(mFpKeystore);
-            mFpWakeAndUnlock.getParent().removePreference(mFpWakeAndUnlock);
         }
     }
 }
