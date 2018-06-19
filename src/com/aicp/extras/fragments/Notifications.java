148ea289dc0a7e7b6276ec2e398a8e2db2694cc0
qjohn <townex22@gmail.com>
2018-06-20 04:18:44 +0700

    AE: Disable animation option if ticker disabled
    Change-Id: I25e398b3ac965b8a6bed9b8c1ce39962cb4d33f4

--- src/com/aicp/extras/fragments/Notifications.java
+++ src/com/aicp/extras/fragments/Notifications.java
@@ -17,16 +17,55 @@
 
 package com.aicp.extras.fragments;
 
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.gear.preference.SystemSettingIntListPreference;
 
-public class Notifications extends BaseSettingsFragment {
+public class Notifications extends BaseSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TICKER_MODE = "status_bar_show_ticker";
+    private static final String TICKER_MODE_ANIMATION =
+            "status_bar_ticker_animation_mode";
+
+    private SystemSettingIntListPreference mTickerMode;
+    private SystemSettingIntListPreference mTickerModeAnimation;
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.notifications;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mTickerMode =
+                (SystemSettingIntListPreference) findPreference(TICKER_MODE);
+        mTickerMode.setOnPreferenceChangeListener(this);
+
+        mTickerModeAnimation =
+                (SystemSettingIntListPreference) findPreference(TICKER_MODE_ANIMATION);
+        int tickerMode = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_TICKER, 0,
+                UserHandle.USER_CURRENT);
+        mTickerModeAnimation.setEnabled(tickerMode > 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mTickerMode)) {
+             int value = Integer.parseInt((String) newValue);
+             mTickerModeAnimation.setEnabled(value > 0);
+             return true;
+        }
+        return false;
+    }
 }
