76aecb17c9df89d8bd2f89e0cd4f72dad642b85e
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2018-07-27 18:43:08 +0300

    ae: App circle sidebar [2/2]
    @xanaxdroid Forward ported for nougat
    
    @eyosen adapted to AICP ae framework
     - use our preferences lib
     - extend existing chooser for app selection
     - add enable to show trigger region toggle
    
    Change-Id: I6d0b4aff71f3605b36adf259421e62d0c19242d1

---
+++ src/com/aicp/extras/fragments/AppCircleBar.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
+import android.provider.Settings;
+
+import com.aicp.extras.R;
+import com.aicp.extras.BaseSettingsFragment;
+
+public class AppCircleBar extends BaseSettingsFragment implements
+        OnPreferenceClickListener {
+    private static final String TAG = "AppCircleSidebar";
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.app_circlebar;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+      return true;
+    }
+}
