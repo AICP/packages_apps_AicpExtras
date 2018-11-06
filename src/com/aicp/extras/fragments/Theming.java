d43e49727290b62f93bdb4b01b7cce6e6a5baba6
SpiritCroc <dev@spiritcroc.de>
2018-11-06 13:11:30 +0100

    More dark themes [3/3]
    - Dark notifications
    - Semi-transparent themes
    
    Change-Id: I673ac36ea37df2c564288bfb2dee112e4d06990f

--- src/com/aicp/extras/fragments/Theming.java
+++ src/com/aicp/extras/fragments/Theming.java
@@ -17,14 +17,57 @@
 
 package com.aicp.extras.fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.extras.utils.Util;
 
-public class Theming extends BaseSettingsFragment {
+public class Theming extends BaseSettingsFragment implements Preference.OnPreferenceChangeListener {
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.theming;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        findPreference(Settings.System.THEMING_BASE).setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (Settings.System.THEMING_BASE.equals(preference.getKey())) {
+            // If notifications are themed (both previously or as a result),
+            // we need to restart SystemUI for changes to have effect
+            if (hasDarkNotifications(Integer.parseInt((String) newValue)) ||
+                    hasDarkNotifications(Settings.System.getInt(getActivity().getContentResolver(),
+                            Settings.System.THEMING_BASE, 0))) {
+                // Keep a context even if activity gets closed
+                final Context appContext = getActivity().getApplicationContext();
+                mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Util.restartSystemUi(appContext);
+                        }
+                });
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean hasDarkNotifications(int baseThemePref) {
+            return baseThemePref >= 3 && baseThemePref <= 6;
     }
 
 }
