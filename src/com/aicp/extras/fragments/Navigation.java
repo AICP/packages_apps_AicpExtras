3a9f42112a23ab69558a7b0b8840ccc37ea70728
Ali B <abittin@gmail.com>
2018-11-17 18:00:00 +0300

    ae: reorganize navigation options
    Change-Id: Iaf14a7b0c4f03aa844802724abf43c59dff7a1aa

--- src/com/aicp/extras/fragments/Navigation.java
+++ src/com/aicp/extras/fragments/Navigation.java
@@ -16,13 +16,65 @@
 
 package com.aicp.extras.fragments;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.extras.preference.MasterSwitchPreference;
 
-public class Navigation extends BaseSettingsFragment {
+public class Navigation extends BaseSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_NAVIGATION_BAR_ENABLED =
+            "navigation_bar_enabled";
+
+    private MasterSwitchPreference mNavBarPreference;
+
+    private boolean mNavbarEnabled;
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.navigation;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ContentResolver resolver = getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mNavBarPreference =
+                (MasterSwitchPreference) findPreference(KEY_NAVIGATION_BAR_ENABLED);
+
+        mNavbarEnabled = Settings.System.getInt(resolver,
+                Settings.System.NAVIGATION_BAR_ENABLED, 0) !=0;
+
+        updateButtonLights(mNavbarEnabled);
+        mNavBarPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNavBarPreference) {
+            updateButtonLights((Boolean) newValue);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateButtonLights(Boolean navbarstate) {
+        ContentResolver resolver = getContentResolver();
+        Settings.System.putInt(resolver, Settings.System.BUTTON_BRIGHTNESS_ENABLED,
+                navbarstate ? 0 : 1);
+        Settings.System.putInt(resolver, Settings.System.BUTTON_BRIGHTNESS,
+                navbarstate ? 0 : 180);
     }
 }
