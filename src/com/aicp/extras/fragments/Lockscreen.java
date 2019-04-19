08b0681feabff1484e9578ca9dfff1af3e85f6e6
Ali B <abittin@gmail.com>
2019-04-19 23:35:55 +0300

    ae: Adapt max font size to LS clock selection
    Font sizes bigger than 64dp make textclock
    wacky. So limit seekbar max value
    to 64 when textclock style is selected and
    to 108 otherwise.
    
    Change-Id: Iac5688d53c48b94896dddf1dc4f4bea6a9a1b7ef

--- src/com/aicp/extras/fragments/Lockscreen.java
+++ src/com/aicp/extras/fragments/Lockscreen.java
@@ -21,6 +21,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
@@ -28,12 +30,23 @@ import android.support.v14.preference.SwitchPreference;
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 
-public class Lockscreen extends BaseSettingsFragment {
+import com.aicp.gear.preference.SystemSettingIntListPreference;
+import com.aicp.gear.preference.SystemSettingSeekBarPreference;
+
+public class Lockscreen extends BaseSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String FP_SUCCESS_VIBRATION = "fingerprint_success_vib";
+    private static final String PREF_LS_CLOCK_SELECTION = "lockscreen_clock_selection";
+    private static final String PREF_LS_CLOCK_FONTSIZE = "lockclock_font_size";
+
+    private static final int CLOCK_MAXSIZE = 108;
+    private static final int TEXT_CLOCK_MAXSIZE = 64;
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintVib;
+    private SystemSettingIntListPreference mLockClockStylePref;
+    private SystemSettingSeekBarPreference mLockClockFontPref;
 
     @Override
     protected int getPreferenceResource() {
@@ -47,7 +60,8 @@ public class Lockscreen extends BaseSettingsFragment {
         ContentResolver resolver = getActivity().getContentResolver();
 
         try {
-            mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+            mFingerprintManager = (FingerprintManager) getActivity().
+                                getSystemService(Context.FINGERPRINT_SERVICE);
         } catch (Exception e) {
             //ignore
         }
@@ -56,6 +70,36 @@ public class Lockscreen extends BaseSettingsFragment {
 
         if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()){
             mFingerprintVib.getParent().removePreference(mFingerprintVib);
+        }
+
+        // Lockscreen clock font size
+        mLockClockFontPref = (SystemSettingSeekBarPreference)
+                              prefSet.findPreference(PREF_LS_CLOCK_FONTSIZE);
+
+        // Lockscreen clock selection
+        mLockClockStylePref = (SystemSettingIntListPreference)
+                              prefSet.findPreference(PREF_LS_CLOCK_SELECTION);
+        int lockClockStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT);
+        updateFontSizeMax(lockClockStyle);
+        mLockClockStylePref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockClockStylePref) {
+            int value = Integer.parseInt((String) newValue);
+            updateFontSizeMax(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateFontSizeMax(int clockStyle){
+        if (clockStyle == 14) {
+            mLockClockFontPref.setMax(TEXT_CLOCK_MAXSIZE);
+        } else {
+            mLockClockFontPref.setMax(CLOCK_MAXSIZE);
         }
     }
 }
