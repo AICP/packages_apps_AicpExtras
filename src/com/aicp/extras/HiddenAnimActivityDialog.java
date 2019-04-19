bf8a0186ae43812b22b7c72cedd36a23f422906d
Michele Bono <bono.michele94@gmail.com>
2019-04-19 13:34:55 +0200

    AE: get our yoga back
    Change-Id: Ia66accf3a6a845754476f35a9ea1471bf607c335

---
+++ src/com/aicp/extras/HiddenAnimActivityDialog.java
@@ -0,0 +1,42 @@
+package com.aicp.extras;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.Window;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+public class HiddenAnimActivityDialog {
+
+    Activity activity;
+    Dialog dialog;
+
+    public HiddenAnimActivityDialog(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void showDialog() {
+        dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.hiddenactivity_dialog_layout);
+
+        final ImageView imgView = dialog.findViewById(R.id.imageView_1);
+
+        GlideDrawableImageViewTarget imgViewTarget = new GlideDrawableImageViewTarget(imgView);
+        Glide.with(activity)
+                .load("https://i.imgur.com/ZkBoyNA.jpg")
+                .placeholder(R.drawable.glide_loading)
+                .error(R.drawable.glide_error)
+                .into(imgViewTarget);
+
+        dialog.show();
+    }
+
+    public void hideDialog(){
+        dialog.dismiss();
+    }
+
+}
