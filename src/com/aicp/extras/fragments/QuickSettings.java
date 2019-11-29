b6b12141a0855abf189f6ce8cb5a8e6e1a7da31a
Ali B <abittin@gmail.com>
2019-11-29 19:38:51 +0300

    ae: dynamically manage system info entries
    Not all devices support all available options.
    Update system info preference based on defined
    device overlays.
    
    Change-Id: Id2f59552d488b63757f3db2b2f8f94d0860617fa

--- src/com/aicp/extras/fragments/QuickSettings.java
+++ src/com/aicp/extras/fragments/QuickSettings.java
@@ -19,23 +19,31 @@ package com.aicp.extras.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
-
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 import com.aicp.gear.preference.SystemSettingSeekBarPreference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSettings extends BaseSettingsFragment
     implements OnPreferenceChangeListener {
 
+    private static final boolean DEBUG = false;
     private static final String QS_QUICKBAR_COLUMNS_AUTO = "qs_quickbar_columns_auto";
     private static final String QS_QUICKBAR_COLUMNS_COUNT = "qs_quickbar_columns";
+    private static final String SYSTEM_INFO = "qs_system_info";
 
+    private ListPreference mSYSInfo;
     private SwitchPreference mQQSColsAuto;
     private SystemSettingSeekBarPreference mQQSColsCount;
 
@@ -44,14 +52,15 @@ public class QuickSettings extends BaseSettingsFragment
         return R.xml.quick_settings;
     }
 
-    /*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mQQSColsAuto = (SwitchPreference) findPreference(QS_QUICKBAR_COLUMNS_AUTO);
+        mSYSInfo = (ListPreference) findPreference(SYSTEM_INFO);
+        configureSystemInfo();
+/*        mQQSColsAuto = (SwitchPreference) findPreference(QS_QUICKBAR_COLUMNS_AUTO);
         mQQSColsCount = (SystemSettingSeekBarPreference) findPreference(QS_QUICKBAR_COLUMNS_COUNT);
 
         boolean qqsColsAutoEnabled = Settings.System.getInt(resolver,
@@ -59,8 +68,8 @@ public class QuickSettings extends BaseSettingsFragment
         mQQSColsAuto.setChecked(qqsColsAutoEnabled);
         mQQSColsCount.setEnabled(!qqsColsAutoEnabled);
         mQQSColsAuto.setOnPreferenceChangeListener(this);
+        */
     }
-    */
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -77,5 +86,30 @@ public class QuickSettings extends BaseSettingsFragment
         }
         */
         return false;
+    }
+
+    private void configureSystemInfo() {
+        Resources res = getResources();
+        String[] entriesArray = res.getStringArray(R.array.qs_system_info_entries);
+        String[] valuesArray = res.getStringArray(R.array.qs_system_info_values);
+        String[] checksArray = res.getStringArray(R.array.qs_system_info_checks);
+        List<String> entries = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        entries.add(entriesArray[0]);
+        values.add(valuesArray[0]);
+        int count = valuesArray.length;
+        for (int i = 1 ; i < count ; i++) {
+            int resID = res.getIdentifier(checksArray[i-1], "string", "android");
+            if (DEBUG) Log.d("systemInfo", "resID= " + resID);
+            if (resID > 0 && !res.getString(resID).isEmpty()) {
+                  if (DEBUG) Log.d("systemInfo", "sysPath= " + res.getString(resID));
+                  entries.add(entriesArray[i]);
+                  values.add(valuesArray[i]);
+            }
+        }
+        mSYSInfo.setEntries(entries.toArray(new String[entries.size()]));
+        mSYSInfo.setEntryValues(values.toArray(new String[values.size()]));
+        if (entries.size() < 2) mSYSInfo.getParent().removePreference(mSYSInfo);
     }
 }
