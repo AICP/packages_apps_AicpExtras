6a3eef757ff1072e8dcd9990b2e3c5833bc10cdf
Chris <chris@cypheros.co>
2020-04-11 17:31:42 +0300

    Navbar: Implement button customizations
    Forward ports all button customization features to Pie
    
    Credits: Paranoid Android for the initial implementation here efab91c
    Original Author: Carlo Savignano <carlosavignano@aospa.co>
    
    **DU Edits**
    - Move everything into one fragment
    - Hide buttons if no hw keys config
    - Move navigation bar switch to navigation category
    - Make everything play nice with each other
    
    Change-Id: Id5ed488db5dc19dce4f9b69767673f8beccebc63

--- src/com/aicp/extras/fragments/Navigation.java
+++ src/com/aicp/extras/fragments/Navigation.java
@@ -17,19 +17,28 @@
 package com.aicp.extras.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.extras.preference.SystemSettingMasterSwitchPreference;
 
 import com.android.internal.util.hwkeys.ActionUtils;
 
-public class Navigation extends BaseSettingsFragment {
+public class Navigation extends BaseSettingsFragment implements
+            Preference.OnPreferenceChangeListener {
 
     private static final String KEY_KILLAPP_LONGPRESS_BACK = "kill_app_longpress_back";
     private static final String KEY_SWAP_HW_NAVIGATION_KEYS = "swap_navigation_keys";
+    private static final String KEY_NAVIGATION_BAR_ENABLED = "navigation_bar_show_new";
+
+    private SystemSettingMasterSwitchPreference mNavigationBar;
+    private boolean mIsNavSwitchingMode = false;
+
+    private Handler mHandler;
 
     @Override
     protected int getPreferenceResource() {
@@ -46,9 +55,32 @@ public class Navigation extends BaseSettingsFragment {
         final boolean hwkeysSupported = ActionUtils.isHWKeysSupported(getActivity());
         SwitchPreference longPressBackToKill = (SwitchPreference) findPreference(KEY_KILLAPP_LONGPRESS_BACK);
         SwitchPreference swapHWNavKeys = (SwitchPreference) findPreference(KEY_SWAP_HW_NAVIGATION_KEYS);
+
         if (needsNavbar || !hwkeysSupported) {
-            prefScreen.removePreference(longPressBackToKill);
-            prefScreen.removePreference(swapHWNavKeys);
+            if (swapHWNavKeys != null) swapHWNavKeys.getParent().removePreference(swapHWNavKeys);
+            if (longPressBackToKill != null) longPressBackToKill.getParent().removePreference(longPressBackToKill);
         }
+
+        mNavigationBar = (SystemSettingMasterSwitchPreference) findPreference(KEY_NAVIGATION_BAR_ENABLED);
+        mNavigationBar.setOnPreferenceChangeListener(this);
+        mHandler = new Handler();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBar) {
+            boolean value = (Boolean) objValue;
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
+        return false;
     }
 }
