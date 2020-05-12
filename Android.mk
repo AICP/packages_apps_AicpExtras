177a0e5122f1b5e312e4d17a97eeea6726877f71
Ali B <abittin@gmail.com>
2020-05-12 20:48:25 +0300

    AicpExtras: Add privapp permissions xml
    Change-Id: I4fca5ec22200cac7ca9196ebfc77d283abeb9114

--- Android.mk
+++ Android.mk
@@ -55,10 +55,20 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.aicp.extras-ext.xml
 
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.aicp.extras-ext.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 
