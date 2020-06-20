032a864eb626707648064f0ece5c3353b921b9c8
Ali B <abittin@gmail.com>
2020-06-20 16:58:51 +0300

    ae: Add DND & Rate settings to flash on call [3/3]
    Change-Id: Ie66990de55ad6c180178762d2681b3a8e88d2f85

--- src/com/aicp/extras/fragments/Notifications.java
+++ src/com/aicp/extras/fragments/Notifications.java
@@ -17,20 +17,35 @@
 
 package com.aicp.extras.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.provider.Settings;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.SwitchPreference;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 import com.aicp.extras.utils.Util;
+import com.aicp.gear.preference.SystemSettingIntListPreference;
+import com.aicp.gear.preference.SystemSettingSeekBarPreference;
 import com.android.internal.util.aicp.DeviceUtils;
 
-public class Notifications extends BaseSettingsFragment {
+public class Notifications extends BaseSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
+    private static final String CAT_NOTIFICATION_FLASHLIGHT = "notification_flash";
+    private static final String PREF_FLASHLIGHT_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASHLIGHT_ON_CALL_WAITING = "flashlight_on_call_waiting";
+    private static final String PREF_FLASHLIGHT_ON_CALL_IGNORE_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
+
+    private SwitchPreference mFlashOnCallWaiting;
+    private SwitchPreference mFlashOnCallIgnoreDND;
+    private SystemSettingIntListPreference mFlashOnCall;
+    private SystemSettingSeekBarPreference mFlashOnCallRate;
 
     @Override
     protected int getPreferenceResource() {
@@ -44,14 +59,19 @@ public class Notifications extends BaseSettingsFragment {
         Util.requireFullStatusbar(getActivity(),
                 findPreference(Settings.System.STATUS_BAR_SHOW_TICKER));
 
-        if (!DeviceUtils.deviceSupportsFlashLight(getActivity())) {
-            getPreferenceScreen().removePreference(findPreference(
-                    Settings.System.FLASHLIGHT_ON_CALL));
-        }
+        mFlashOnCallWaiting = (SwitchPreference) getPreferenceScreen().findPreference(PREF_FLASHLIGHT_ON_CALL_WAITING);
+        mFlashOnCallIgnoreDND = (SwitchPreference) getPreferenceScreen().findPreference(PREF_FLASHLIGHT_ON_CALL_IGNORE_DND);
+        mFlashOnCallRate = (SystemSettingSeekBarPreference) getPreferenceScreen().findPreference(PREF_FLASHLIGHT_ON_CALL_RATE);
+
+        mFlashOnCall = (SystemSettingIntListPreference) getPreferenceScreen().findPreference(PREF_FLASHLIGHT_ON_CALL);
+        boolean optionEnabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0) != 0;
+        updateDependencies(optionEnabled);
+        mFlashOnCall.setOnPreferenceChangeListener(this);
 
         if (!DeviceUtils.deviceSupportsFlashLight(getActivity())) {
             getPreferenceScreen().removePreference(findPreference(
-                    Settings.System.FLASHLIGHT_ON_CALL_WAITING));
+                    CAT_NOTIFICATION_FLASHLIGHT));
         }
 
         boolean alertSliderAvailable = getActivity().getResources().getBoolean(
@@ -59,5 +79,22 @@ public class Notifications extends BaseSettingsFragment {
         if (!alertSliderAvailable)
             getPreferenceScreen().removePreference(findPreference(ALERT_SLIDER_PREF));
 
+    }
+
+    private void updateDependencies(boolean enabled) {
+        mFlashOnCallWaiting.setEnabled(enabled);
+        mFlashOnCallIgnoreDND.setEnabled(enabled);
+        mFlashOnCallRate.setEnabled(enabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getContentResolver();
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            updateDependencies(value != 0);
+            return true;
+        }
+        return false;
     }
 }
