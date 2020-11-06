22131fcc011a5d8be84fdae8125a141bb1fd569c
Ali B <abittin@gmail.com>
2020-11-06 14:21:32 +0300

    ae: bring back headsup timeout and snooze
    Change-Id: I7bac9173670dff765482c7aae58b5ffda14cc13d

--- src/com/aicp/extras/fragments/HeadsUpOptions.java
+++ src/com/aicp/extras/fragments/HeadsUpOptions.java
@@ -20,11 +20,10 @@ package com.aicp.extras.fragments;
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 
-public class HeadsUpOptions {/*
 public class HeadsUpOptions extends BaseSettingsFragment {
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.heads_up_options;
-    }*/
+    }
 }
