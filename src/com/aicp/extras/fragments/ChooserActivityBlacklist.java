7ac988cba164476334310de06b73069bc717c9ab
Ali B <abittin@gmail.com>
2020-11-07 14:30:21 +0300

    ae: bring back hostname preference
    Change-Id: Ia1eeae6bc6a5dd25c010f145460e99527e49f1d5

--- src/com/aicp/extras/fragments/ChooserActivityBlacklist.java
+++ src/com/aicp/extras/fragments/ChooserActivityBlacklist.java
@@ -51,7 +51,6 @@ import com.aicp.extras.utils.PackageListAdapter.PackageItem;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.aicp.extras.R;
 
-public class ChooserActivityBlacklist {/*
 public class ChooserActivityBlacklist extends BaseSettingsFragment implements
         Preference.OnPreferenceClickListener {
 
@@ -151,13 +150,13 @@ public class ChooserActivityBlacklist extends BaseSettingsFragment implements
     /**
      * Application class
      */
-/*    protected static class Package {
+    protected static class Package {
         public String name;
         /**
          * Stores all the application values in one call
          * @param name
          */
-  /*      public Package(String name) {
+        public Package(String name) {
             this.name = name;
         }
 
@@ -285,5 +284,5 @@ public class ChooserActivityBlacklist extends BaseSettingsFragment implements
         if (DEBUG) Log.v(TAG, "blackStringSaved: " + value);
         Settings.System.putString(getActivity().getContentResolver(),
                 mBlacklistPackageStore, value);
-    }*/
+    }
 }
