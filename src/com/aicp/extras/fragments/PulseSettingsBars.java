e10da1f3c6de9ac9a21af5097268b27b41bd68d5
Ali B <abittin@gmail.com>
2020-12-19 13:57:31 +0300

    ae: bring back pulse settings
    Change-Id: I6df6779f235cc2cd8abc4b5eed5af6ce6bcd36fe

--- src/com/aicp/extras/fragments/PulseSettingsBars.java
+++ src/com/aicp/extras/fragments/PulseSettingsBars.java
@@ -20,11 +20,10 @@ package com.aicp.extras.fragments;
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 
-public class PulseSettingsBars {/*
 public class PulseSettingsBars extends BaseSettingsFragment {
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.pulse_settings_bars;
-    }*/
+    }
 }
