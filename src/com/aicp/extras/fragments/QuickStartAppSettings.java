1b57ca8b627e0ee286805813085e4806337d8757
cjybyjk <cjybyjk@zjnu.edu.cn>
2021-04-05 23:53:36 +0800

    Add GamingMode support [3/3]
    ** AICPify
    
    Change-Id: I27960bbbf982a4cd855524baa743ed6647a2f0be
    Reviewed-on: https://review.exthmui.cn/c/android_packages_apps_exTHmSettings/+/209
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

---
+++ src/com/aicp/extras/fragments/QuickStartAppSettings.java
@@ -0,0 +1,49 @@
+package com.aicp.extras.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.aicp.extras.R;
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.gear.preference.AicpPreferenceFragment;
+
+public class QuickStartAppSettings extends BaseSettingsFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.settings_gaming_qs_app;
+    }
+
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.settings_gaming_qs_app);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+}
