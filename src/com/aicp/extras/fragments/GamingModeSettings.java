e6d2fafcd6d8ea775ddf169a9bf96aed1e7c58b2
cjybyjk <cjybyjk@zjnu.edu.cn>
2021-04-05 23:53:36 +0800

    Add GamingMode support [3/3]
    ** AICPify
    
    Change-Id: I27960bbbf982a4cd855524baa743ed6647a2f0be
    Reviewed-on: https://review.exthmui.cn/c/android_packages_apps_exTHmSettings/+/209
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

---
+++ src/com/aicp/extras/fragments/GamingModeSettings.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2020 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
+
+import com.aicp.extras.R;
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.gear.preference.AicpPreferenceFragment;
+
+import java.util.ArrayList;
+
+import com.aicp.extras.preference.PackageListPreference;
+import com.aicp.gear.preference.SystemSettingSeekBarPreference;
+
+public class GamingModeSettings extends BaseSettingsFragment {
+
+    private PackageListPreference mGamingPrefList;
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.settings_gaming;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
+        mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+    }
+}
