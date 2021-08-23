d3d4c9803a0e2d6e03f4de84debe3d5574bcbcd6
Ali B <abittin@gmail.com>
2021-08-23 20:21:13 +0300

    ae: Implement Aggressive Battery [2/2]
    Change-Id: Ia68c3544a668f0ec54fc82f10c64cff0045a357d

---
+++ src/com/aicp/extras/fragments/AggressiveBattery.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.fragments;
+
+import androidx.preference.Preference;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class AggressiveBattery extends BaseSettingsFragment {
+
+    private static final String TAG = "AggressiveBattery";
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.aggressive_battery;
+    }
+
+/*    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = (Context) getActivity();
+
+    }*/
+}
