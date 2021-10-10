syberia-project/platform_packages_apps_SyberiaSettings

GamingMode: FPS info overlay [3/3]
Change-Id: Id773eb0a1dee0ff398cd0372cbc701cc16791165
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/syberia/settings/fragments/GamingModeSettings.java
@@ -22,6 +22,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference;
@@ -41,11 +42,13 @@ public class GamingModeSettings extends SettingsPreferenceFragment implements On
 
     private PackageListPreference mGamingPrefList;
     private SwitchPreference mUseMenuSwitch;
+    private SwitchPreference mShowFPS;
     private Preference mDanmaku;
     private Preference mQapps;
     private SystemSettingSeekBarPreference mOpacity;
 
     private boolean performance_supported;
+    private boolean mFpsInfoSupported;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -61,14 +64,29 @@ public class GamingModeSettings extends SettingsPreferenceFragment implements On
         performance_supported = getResources().getBoolean(
                     com.android.internal.R.bool.config_gamingmode_performance);
 
+        final String fpsInfoSysNode = getResources().getString(
+                com.android.internal.R.string.config_fpsInfoSysNode);
+        mFpsInfoSupported = !TextUtils.isEmpty(fpsInfoSysNode);
+
         if (!performance_supported) {
             prefScreen.removePreference(perfCat);
         }
 
         mUseMenuSwitch = (SwitchPreference) findPreference("gaming_mode_use_overlay_menu");
+        mShowFPS = (SwitchPreference) findPreference("gaming_mode_fps_info");
         mDanmaku = (Preference) findPreference("gaming_mode_notification_danmaku");
         mQapps = (Preference) findPreference("gaming_mode_quick_start_apps");
         mOpacity = (SystemSettingSeekBarPreference) findPreference("gaming_mode_menu_opacity");
+
+        boolean fpsEnabled = Settings.System.getInt(getContentResolver(),
+                            Settings.System.GAMING_MODE_SHOW_FPSINFO, 0) == 1;
+
+        if (mFpsInfoSupported) {
+            mShowFPS.setChecked(fpsEnabled);
+            mShowFPS.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(mShowFPS);
+        }
 
         boolean menuEnabled = Settings.System.getInt(getContentResolver(),
                             Settings.System.GAMING_MODE_USE_OVERLAY_MENU, 1) == 1;
@@ -93,6 +111,13 @@ public class GamingModeSettings extends SettingsPreferenceFragment implements On
             mDanmaku.setEnabled(value);
             mQapps.setEnabled(value);
             mOpacity.setEnabled(value);
+//            if (mFpsInfoSupported)
+//                mShowFPS.setEnabled(value);
+            return true;
+        } else if (preference == mShowFPS) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.GAMING_MODE_SHOW_FPSINFO,
+                    value ? 1 : 0);
             return true;
         }
         return false;

AICP/packages_apps_AicpExtras

src/com/aicp/extras/fragments/GamingModeSettings.java
@@ -22,6 +22,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference;
@@ -42,11 +43,13 @@ public class GamingModeSettings extends BaseSettingsFragment implements OnPrefer
 
     private PackageListPreference mGamingPrefList;
     private SwitchPreference mUseMenuSwitch;
+    private SwitchPreference mShowFPS;
     private Preference mDanmaku;
     private Preference mQapps;
     private SystemSettingSeekBarPreference mOpacity;
 
     private boolean performance_supported;
+    private boolean mFpsInfoSupported;
 
     @Override
     protected int getPreferenceResource() {
@@ -65,14 +68,29 @@ public class GamingModeSettings extends BaseSettingsFragment implements OnPrefer
         performance_supported = getResources().getBoolean(
                     com.android.internal.R.bool.config_gamingmode_performance);
 
+        final String fpsInfoSysNode = getResources().getString(
+                com.android.internal.R.string.config_gamingmode_fpsInfoSysNode);
+        mFpsInfoSupported = !TextUtils.isEmpty(fpsInfoSysNode);
+
         if (!performance_supported) {
             prefScreen.removePreference(perfCat);
         }
 
         mUseMenuSwitch = (SwitchPreference) findPreference("gaming_mode_use_overlay_menu");
+        mShowFPS = (SwitchPreference) findPreference("gaming_mode_fps_info");
         mDanmaku = (Preference) findPreference("gaming_mode_notification_danmaku");
         mQapps = (Preference) findPreference("gaming_mode_quick_start_apps");
         mOpacity = (SystemSettingSeekBarPreference) findPreference("gaming_mode_menu_opacity");
+
+        boolean fpsEnabled = Settings.System.getInt(getContentResolver(),
+                            Settings.System.GAMING_MODE_SHOW_FPSINFO, 0) == 1;
+
+        if (mFpsInfoSupported) {
+            mShowFPS.setChecked(fpsEnabled);
+            mShowFPS.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(mShowFPS);
+        }
 
         boolean menuEnabled = Settings.System.getInt(getContentResolver(),
                             Settings.System.GAMING_MODE_USE_OVERLAY_MENU, 1) == 1;
@@ -97,6 +115,13 @@ public class GamingModeSettings extends BaseSettingsFragment implements OnPrefer
             mDanmaku.setEnabled(value);
             mQapps.setEnabled(value);
             mOpacity.setEnabled(value);
+//            if (mFpsInfoSupported)
+//                mShowFPS.setEnabled(value);
+            return true;
+        } else if (preference == mShowFPS) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.GAMING_MODE_SHOW_FPSINFO,
+                    value ? 1 : 0);
             return true;
         }
         return false;