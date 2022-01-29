95c3f4b973721041b46df1942cf90e72b5634c3f
Ali B <abittin@gmail.com>
2022-01-29 17:20:15 +0300

    ae: Bring SlimRecents back
    Change-Id: Iaa00d189bf01139234d681a22f6616ddee5555bd

--- src/com/aicp/extras/fragments/SlimRecentsBlacklist.java
+++ src/com/aicp/extras/fragments/SlimRecentsBlacklist.java
@@ -50,7 +50,7 @@ import com.aicp.extras.utils.PackageListAdapter.PackageItem;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.aicp.extras.R;
 
-public class SlimRecentsBlacklist {/*extends BaseSettingsFragment implements
+public class SlimRecentsBlacklist extends BaseSettingsFragment implements
         Preference.OnPreferenceClickListener {
 
     private static final String TAG = "SlimRecentsBlacklist";
@@ -144,13 +144,13 @@ public class SlimRecentsBlacklist {/*extends BaseSettingsFragment implements
 
     /**
      * Application class
-     *//*
+     */
     private static class Package {
         public String name;
         /**
          * Stores all the application values in one call
          * @param name
-         *//*
+         */
         public Package(String name) {
             this.name = name;
         }
@@ -277,5 +277,5 @@ public class SlimRecentsBlacklist {/*extends BaseSettingsFragment implements
         }
         Settings.System.putString(getActivity().getContentResolver(),
                 setting, value);
-    }*/
+    }
 }
