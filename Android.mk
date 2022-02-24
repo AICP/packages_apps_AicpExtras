432caa037aa6e912800c1bad3a104723a1d67244
Semavi Ulusoy <doc.divxm@gmail.com>
2022-02-25 00:09:42 +0300

    AE: not PRODUCT but SYSTEM_EXT
    Change-Id: I76b83f122bb08647f945a53960faeea50ab076a6

--- Android.mk
+++ Android.mk
@@ -67,7 +67,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp_whitelist_com.aicp.extras-ext.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
