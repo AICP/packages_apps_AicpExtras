fe07eed31db9f558c33315c1829c4bbd1d3d0818
George Zacharia <george.zcharia@gmail.com>
2022-08-03 22:17:31 +0530

    ae: Add a tile for logging all the shit
    Not much but keep it within AICP Extras
    Change-Id: I97738c8202c7e7285b0daf0479dea900da3ac4c1

---
+++ src/com/aicp/extras/tiles/LogThatShitTile.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2022 The Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.tiles;
+
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+import com.aicp.extras.SettingsActivity;
+
+public class LogThatShitTile extends TileService {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Intent logThatShit = new Intent(this, SettingsActivity.class);
+        logThatShit.putExtra(":android:show_fragment", "com.aicp.extras.fragments.LogIt");
+        logThatShit.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(logThatShit);
+    }
+}
