221ae747e5e6b2012eda1add4fd6203a6791478e
Julian Veit <claymore1298@gmail.com>
2022-10-29 17:36:56 +0200

    ae: re-enable Statusbar Logo
    Change-Id: I1c8a33b02603e2721716a21ab2c46b5e8a63e7a0

--- src/com/aicp/extras/fragments/StatusBarLogo.java
+++ src/com/aicp/extras/fragments/StatusBarLogo.java
@@ -20,11 +20,10 @@ package com.aicp.extras.fragments;
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
 
-public class StatusBarLogo {/* extends BaseSettingsFragment {
+public class StatusBarLogo extends BaseSettingsFragment {
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.status_bar_logo_settings;
     }
- */
 }
