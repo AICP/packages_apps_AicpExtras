aeaa53c49a4fc504416d6c1b23bfbb322e3161f8
SpiritCroc <dev@spiritcroc.de>
2025-07-30 22:53:58 +0200

    AE slim recents V / 15
    Change-Id: Ie742beb08bc1298e6f576eb70a3777b4e2e26e32

--- src/com/aicp/extras/dslv/ActionListViewSettings.java
+++ src/com/aicp/extras/dslv/ActionListViewSettings.java
@@ -580,10 +580,10 @@ public class ActionListViewSettings extends ListFragment implements
             case SHAKE_EVENTS_DISABLED:
                 return ActionHelper.getDisabledShakeApps(mActivity);
 
+        */
             case RECENT_APP_SIDEBAR:
                 return ActionHelper.getRecentAppSidebarConfigWithDescription(
                         mActivity, mActionValuesKey, mActionEntriesKey);
-        */
         }
         return null;
     }
