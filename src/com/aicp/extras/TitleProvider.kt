9eaaedcf8a4e90a29ef645157c7bb5e68f9d32e9
Julian Veit <claymore1298@gmail.com>
2026-02-22 21:28:15 +0000

    AE: migrate to kotlin
    Change-Id: Ide5161ae752c3fe7d40c27609c37ef7d15de4f69

--- src/com/aicp/extras/TitleProvider.kt
+++ src/com/aicp/extras/TitleProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 AICP
+ * Copyright (C) 2017-2026 AICP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.aicp.extras
 
-package com.aicp.extras;
-
-public interface TitleProvider {
-    CharSequence getTitle();
+interface TitleProvider {
+    fun getTitle(): CharSequence
 }
+
