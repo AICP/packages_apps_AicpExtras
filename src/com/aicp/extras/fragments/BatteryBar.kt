1e35b248d0e2d261b5745e71e68a3fa3d0f22a9a
Julian Veit <claymore1298@gmail.com>
2026-02-25 16:13:54 +0000

    AE: migrate fragments to kotlin
    Change-Id: Icdb658fb32920e5e22d2ee56c3ee36b37e0ee9e2

--- src/com/aicp/extras/fragments/BatteryBar.kt
+++ src/com/aicp/extras/fragments/BatteryBar.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 AICP
+ * Copyright (C) 2018-2026 Android Ice Cold Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
+package com.aicp.extras.fragments
 
-package com.aicp.extras.fragments;
+import com.aicp.extras.BaseSettingsFragment
+import com.aicp.extras.R
 
-import androidx.preference.Preference;
+class BatteryBar : BaseSettingsFragment() {
 
-import com.aicp.extras.BaseSettingsFragment;
-import com.aicp.extras.R;
-
-public class SlimRecentAppSidebar extends BaseSettingsFragment {
-
-    @Override
-    protected int getPreferenceResource() {
-        return R.xml.slim_recent_app_sidebar;
+    override fun getPreferenceResource(): Int {
+        return R.xml.battery_bar
     }
 }
+
