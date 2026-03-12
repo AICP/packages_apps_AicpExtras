2e8ac8586a9fa8c8023fb2351fd75f6f058368e9
Julian Veit <claymore1298@gmail.com>
2026-03-12 00:04:14 +0000

    AE: Add customizable clock date options in status bar settings
    converted to kotlin by @Claymore1297
    
    Change-Id: I54a9df2eee68b5656dbd495645c5b5f4be1cf538
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/aicp/extras/fragments/StatusBarClockSettings.kt
+++ src/com/aicp/extras/fragments/StatusBarClockSettings.kt
@@ -1,179 +1,174 @@
 /*
- * Copyright (C) 2018-2026 Android Ice Cold Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * SPDX-FileCopyrightText: 2018-2026 Android Ice Cold Project
+ * SPDX-License-Identifier: Apache-2.0
+*/
 package com.aicp.extras.fragments
 
 import android.os.Bundle
+import android.os.UserHandle
+import android.provider.Settings
+import android.text.format.DateFormat
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import com.aicp.extras.BaseSettingsFragment
 import com.aicp.extras.R
+import com.aicp.gear.preference.SystemSettingListPreference
+import java.util.Date
 
-// class StatusBarClockSettings : BaseSettingsFragment(), Preference.OnPreferenceChangeListener {
-class StatusBarClockSettings : BaseSettingsFragment() {
+class StatusBarClockSettings : BaseSettingsFragment(),
+    Preference.OnPreferenceChangeListener {
 
     companion object {
         private const val TAG = "StatusBarClockSettings"
 
         private const val CLOCK_DATE_FORMAT = "status_bar_clock_date_format"
-        private const val CLOCK_POSITION = "status_bar_clock_position"
+        private const val CLOCK_DATE_POSITION = "status_bar_clock_date_position"
+        private const val CLOCK_DATE_DISPLAY = "status_bar_clock_date_display"
+        private const val CLOCK_DATE_STYLE = "status_bar_clock_date_style"
 
         const val CLOCK_DATE_STYLE_LOWERCASE = 1
         const val CLOCK_DATE_STYLE_UPPERCASE = 2
         private const val CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18
     }
 
-    /*
-    private lateinit var mClockPosition: SystemSettingIntListPreference
-    private lateinit var mClockDateFormat: SystemSettingListPreference
-    */
+    private lateinit var mClockDatePosition: SystemSettingListPreference
+    private lateinit var mClockDateFormat: ListPreference
+    private lateinit var mClockDateDisplay: SystemSettingListPreference
+    private lateinit var mClockDateStyle: SystemSettingListPreference
 
-    override fun getPreferenceResource(): Int {
-        return R.xml.status_bar_clock
-    }
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.status_bar_clock, rootKey)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        mClockDateDisplay = findPreference<SystemSettingListPreference>(CLOCK_DATE_DISPLAY)!!
+        mClockDatePosition = findPreference<SystemSettingListPreference>(CLOCK_DATE_POSITION)!!
+        mClockDateStyle = findPreference<SystemSettingListPreference>(CLOCK_DATE_STYLE)!!
+        mClockDateFormat = findPreference<ListPreference>(CLOCK_DATE_FORMAT)!!
 
-        val resolver = requireActivity().contentResolver
-
-        /*
-        mClockPosition = findPreference(CLOCK_POSITION)!!
-
-        mClockDateFormat = findPreference(CLOCK_DATE_FORMAT)!!
+        mClockDateDisplay.onPreferenceChangeListener = this
+        mClockDatePosition.onPreferenceChangeListener = this
+        mClockDateStyle.onPreferenceChangeListener = this
         mClockDateFormat.onPreferenceChangeListener = this
+
+        val dateDisplay = Settings.System.getIntForUser(
+            requireContext().contentResolver,
+            Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY,
+            0,
+            UserHandle.USER_CURRENT
+        )
+
+        mClockDatePosition.isEnabled = dateDisplay > 0
+        mClockDateStyle.isEnabled = dateDisplay > 0
+        mClockDateFormat.isEnabled = dateDisplay > 0
+
         if (mClockDateFormat.value == null) {
             mClockDateFormat.value = "EEE"
         }
 
         parseClockDateFormats()
-        */
     }
 
-    /*
-    override fun onResume() {
-        super.onResume()
-
-        val hasNotch = AicpUtils.hasNotch(requireActivity())
-        val notchType = DeviceUtils.getCutoutType(requireActivity())
-        Log.v(TAG, "notchType: $notchType")
-
-        if (resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-            if (hasNotch && !(notchType == BOUNDS_POSITION_LEFT || notchType == BOUNDS_POSITION_RIGHT)) {
-                mClockPosition.setEntries(R.array.clock_position_entries_notch_rtl)
-                mClockPosition.setEntryValues(R.array.clock_position_values_notch_rtl)
-            } else {
-                mClockPosition.setEntries(R.array.clock_position_entries_rtl)
-                mClockPosition.setEntryValues(R.array.clock_position_values_rtl)
-            }
-        } else if (hasNotch && !(notchType == BOUNDS_POSITION_LEFT || notchType == BOUNDS_POSITION_RIGHT)) {
-            mClockPosition.setEntries(R.array.clock_position_entries_notch)
-            mClockPosition.setEntryValues(R.array.clock_position_values_notch)
-        }
+    override fun getPreferenceResource(): Int {
+        return R.xml.status_bar_clock
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-        val resolver = requireActivity().contentResolver
+        when (preference) {
+            mClockDateDisplay -> {
+                val valInt = (newValue as String).toInt()
+                val enabled = valInt != 0
+                mClockDatePosition.isEnabled = enabled
+                mClockDateStyle.isEnabled = enabled
+                mClockDateFormat.isEnabled = enabled
+                return true
+            }
+            mClockDatePosition, mClockDateStyle -> {
+                parseClockDateFormats()
+                return true
+            }
+            mClockDateFormat -> {
+                val index = mClockDateFormat.findIndexOfValue(newValue as String)
+                if (index == CUSTOM_CLOCK_DATE_FORMAT_INDEX) {
+                    val alert = AlertDialog.Builder(requireActivity())
+                        .setTitle(R.string.status_bar_date_string_edittext_title)
+                        .setMessage(R.string.status_bar_date_string_edittext_summary)
 
-        if (preference == mClockDateFormat) {
-            val index = mClockDateFormat.findIndexOfValue(newValue as String)
+                    val input = EditText(requireActivity())
+                    val oldText = Settings.System.getString(
+                        requireActivity().contentResolver,
+                        Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT
+                    )
+                    oldText?.let { input.setText(it) }
+                    alert.setView(input)
 
-            if (index == CUSTOM_CLOCK_DATE_FORMAT_INDEX) {
-                val alert = AlertDialog.Builder(requireActivity())
-                alert.setTitle(R.string.clock_date_string_edittext_title)
-                alert.setMessage(R.string.clock_date_string_edittext_summary)
-
-                val container = LinearLayout(requireActivity())
-                container.orientation = LinearLayout.VERTICAL
-
-                val lp = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                lp.setMargins(55, 20, 55, 20)
-
-                val input = EditText(requireActivity())
-                val oldText = Settings.System.getString(
-                    resolver,
-                    Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT
-                )
-                if (oldText != null) {
-                    input.setText(oldText)
-                }
-
-                input.layoutParams = lp
-                input.gravity = Gravity.TOP or Gravity.START
-                container.addView(input)
-                alert.setView(container)
-
-                alert.setPositiveButton(R.string.menu_save) { _, _ ->
-                    val value = input.text.toString()
-                    if (value.isNotEmpty()) {
-                        Settings.System.putString(
-                            resolver,
-                            Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT,
-                            value
-                        )
+                    alert.setPositiveButton(R.string.menu_save) { _, _ ->
+                        val value = input.text.toString()
+                        if (value.isNotEmpty()) {
+                            Settings.System.putString(
+                                requireActivity().contentResolver,
+                                Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT,
+                                value
+                            )
+                        }
                     }
-                }
 
-                alert.setNegativeButton(R.string.menu_cancel) { _, _ -> }
-
-                alert.create().show()
-            } else {
-                if (newValue != null) {
+                    alert.setNegativeButton(R.string.cancel) { _, _ -> }
+                    alert.create().show()
+                } else {
                     Settings.System.putString(
-                        resolver,
+                        requireActivity().contentResolver,
                         Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT,
                         newValue as String
                     )
                 }
+                return true
             }
-            return true
         }
         return false
     }
 
-    private fun parseClockDateFormats() {
-        val dateEntries = resources.getStringArray(
-            R.array.clock_date_format_entries_values
+    override fun onResume() {
+        super.onResume()
+        val dateDisplay = Settings.System.getIntForUser(
+            requireContext().contentResolver,
+            Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY,
+            0,
+            UserHandle.USER_CURRENT
         )
 
+        mClockDatePosition.isEnabled = dateDisplay > 0
+        mClockDateStyle.isEnabled = dateDisplay > 0
+        mClockDateFormat.isEnabled = dateDisplay > 0
+        parseClockDateFormats()
+    }
+
+    private fun parseClockDateFormats() {
+        val dateEntries = resources.getStringArray(R.array.status_bar_date_format_entries_values)
         val parsedDateEntries = Array<CharSequence>(dateEntries.size) { "" }
         val now = Date()
         val lastEntry = dateEntries.size - 1
 
-        val dateFormat = Settings.System.getInt(
+        val dateFormat = Settings.System.getIntForUser(
             requireActivity().contentResolver,
             Settings.System.STATUS_BAR_CLOCK_DATE_STYLE,
-            0
+            0,
+            UserHandle.USER_CURRENT
         )
 
         for (i in dateEntries.indices) {
-            if (i == lastEntry) {
-                parsedDateEntries[i] = dateEntries[i]
+            parsedDateEntries[i] = if (i == lastEntry) {
+                dateEntries[i]
             } else {
                 val dateString = DateFormat.format(dateEntries[i], now).toString()
-                val newDate = when (dateFormat) {
+                when (dateFormat) {
                     CLOCK_DATE_STYLE_LOWERCASE -> dateString.lowercase()
                     CLOCK_DATE_STYLE_UPPERCASE -> dateString.uppercase()
                     else -> dateString
                 }
-                parsedDateEntries[i] = newDate
             }
         }
 
         mClockDateFormat.entries = parsedDateEntries
     }
-    */
 }
