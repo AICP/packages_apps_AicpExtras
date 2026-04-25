4e228fa9ceb26f82218e9b19d41ea43db52761d6
mosimchah <mosimchah@gmail.com>
2026-04-25 16:40:13 -0400

    ae: add warning for spoofing
    Change-Id: If8bef758600e75bd54cbde0c05e2e4b295925046

--- src/com/aicp/extras/fragments/SystemBehaviour.kt
+++ src/com/aicp/extras/fragments/SystemBehaviour.kt
@@ -13,8 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright (C) 2017-2026 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.aicp.extras.fragments
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.os.SystemProperties
 import android.provider.Settings
@@ -22,23 +39,23 @@ import androidx.preference.Preference
 import androidx.preference.SwitchPreferenceCompat
 import com.aicp.extras.BaseSettingsFragment
 import com.aicp.extras.R
+import com.aicp.gear.preference.SecureSettingMasterSwitchPreference
 
-class SystemBehaviour : BaseSettingsFragment(),
-    Preference.OnPreferenceChangeListener {
+class SystemBehaviour : BaseSettingsFragment(), Preference.OnPreferenceChangeListener {
 
     companion object {
         private const val KEY_ENABLE_BLURS = "enable_blurs_on_windows"
-        private const val SF_PROP_REQUIRED_FOR_BLUR =
-            "ro.surface_flinger.supports_background_blur"
+        private const val SF_PROP_REQUIRED_FOR_BLUR = "ro.surface_flinger.supports_background_blur"
+        private const val SPOOFING_KEY = "spoofing"
     }
 
+    private lateinit var mEnableSpoofing: SecureSettingMasterSwitchPreference
     private var enableBlurPref: SwitchPreferenceCompat? = null
 
-    override fun getPreferenceResource(): Int =
-        R.xml.system_behaviour
+    override fun getPreferenceResource(): Int = R.xml.system_behaviour
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
 
         enableBlurPref = findPreference(KEY_ENABLE_BLURS)
 
@@ -57,6 +74,13 @@ class SystemBehaviour : BaseSettingsFragment(),
             if (!blurSupported) {
                 pref.isEnabled = false
             }
+        mEnableSpoofing = findPreference(SPOOFING_KEY)!!        
+        mEnableSpoofing.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            if (!mEnableSpoofing.isChecked) {
+                showSpoofingWarningDialog()
+                return@OnPreferenceClickListener true
+            }
+            false
         }
     }
 
@@ -72,12 +96,20 @@ class SystemBehaviour : BaseSettingsFragment(),
             return true
         }
 
-        return false
+    private fun showSpoofingWarningDialog() {
+        AlertDialog.Builder(requireActivity())
+            .setTitle(R.string.spoofing_warning_title)
+            .setMessage(R.string.spoofing_warning)
+            .setPositiveButton(android.R.string.yes) { _, _ ->
+                Settings.Secure.putInt(requireContext().contentResolver, SPOOFING_KEY, 1)
+                mEnableSpoofing.isChecked = true
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
     }
 
     private fun setWindowBlur(disable: Boolean) {
         val context = context ?: return
-
         try {
             Settings.Global.putInt(
                 context.contentResolver,
@@ -89,4 +121,3 @@ class SystemBehaviour : BaseSettingsFragment(),
         }
     }
 }
-
